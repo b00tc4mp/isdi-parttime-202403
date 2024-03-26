7c2f10269b86dea0748b8d26b60324b57e5f0896
Cristian Gil Burgos <crisgilb@hotmail.com>
2024-03-26 17:31:04 +0100

    add new content to the playground CV #84

--- staff/cristian-gil/playground/cv/index.js
+++ staff/cristian-gil/playground/cv/index.js
@@ -1,45 +1,143 @@
+//Título
 var headTitle = document.querySelector('title');
-headTitle.innerText = 'CV - Pepito Grillo';
+headTitle.innerText = 'CURRICULUM VITAE';
 
 var title = document.createElement('h1');
 title.innerText = headTitle.innerText;
 
 document.body.appendChild(title);
 
-var introTitle = document.createElement('h2');
-introTitle.innerText = 'Intro';
 
-document.body.appendChild(introTitle);
+//Nombre
+var introName = document.createElement('h2');
+introName.innerText = 'Pinocho Grillo Disney';
+
+document.body.appendChild(introName);
+
+
+//Breve presentación
+var introIntroduction = document.createElement('h3');
+introIntroduction.innerText = 'Introduction';
+
+document.body.appendChild(introIntroduction);
+
+
 
 var introDescription = document.createElement('p');
-introDescription.innerText = "Soy aficionado a la tecla desde que tengo uso de razón... blah blah blah";
+introDescription.innerText = "Muñero de madera con aspiraciones de convertirme en un niño real busca empleo para afrontar nuevos retos";
 
 document.body.appendChild(introDescription);
 
-var certificationsTitle = document.createElement('h2');
+
+//Certificaciones académicas - lista
+var certificationsTitle = document.createElement('h3');
 certificationsTitle.innerText = 'Certifications';
 
 document.body.appendChild(certificationsTitle);
 
+
+
 var certificationsList = document.createElement('ul');
 
+
 var certificationsItem1 = document.createElement('li');
-certificationsItem1.innerText = 'Scrum Master';
+certificationsItem1.innerText = 'Licenciado en Muñecos de Madera - Universidad Disney (USA)';
 
 certificationsList.appendChild(certificationsItem1);
 
 document.body.appendChild(certificationsList);
 
+
 var certificationsItem2 = document.createElement('li');
-certificationsItem2.innerText = 'Full Stack Development (ISDI Coders)';
+certificationsItem2.innerText = 'Master en Títeres - Escuela oficial de Entretenimiento infantil';
 
 certificationsList.appendChild(certificationsItem2);
 
 document.body.appendChild(certificationsList);
 
-// TODO continue and end CV (Exerience, Projects, ...)
 
-var experienceTitle = document.createElement('h2');
+//Experiencia
+var experienceTitle = document.createElement('h3');
 experienceTitle.innerText = 'Experience';
 
 document.body.appendChild(experienceTitle);
+
+
+var experienceList = document.createElement('ul');
+
+
+var experienceItem1 = document.createElement('li');
+experienceItem1.innerText = 'Muñeco de madera';
+
+experienceList.appendChild(experienceItem1);
+
+document.body.appendChild(experienceList);
+
+
+
+var experienceItem2 = document.createElement('li');
+experienceItem2.innerText = 'Burro de circo';
+
+experienceList.appendChild(experienceItem2);
+
+document.body.appendChild(experienceList);
+
+
+
+var experienceItem3 = document.createElement('li');
+experienceItem3.innerText = 'Alimento de ballena';
+
+experienceList.appendChild(experienceItem3);
+
+document.body.appendChild(experienceList);
+
+
+
+var experienceItem4 = document.createElement('li');
+experienceItem4.innerText = 'Niño real';
+
+experienceList.appendChild(experienceItem4);
+
+document.body.appendChild(experienceList);
+
+
+//Idiomas
+var langTitle = document.createElement('h3');
+langTitle.innerText = 'Languajes';
+
+document.body.appendChild(langTitle);
+
+
+var langList = document.createElement('ol');
+
+
+var langItem1 = document.createElement('li');
+langItem1.innerText = 'English - (Mother tongue)';
+
+langList.appendChild(langItem1);
+
+document.body.appendChild(langList);
+
+
+var langItem2 = document.createElement('li');
+langItem2.innerText = 'Spanish';
+
+langList.appendChild(langItem2);
+
+document.body.appendChild(langList);
+
+
+var langItem3 = document.createElement('li');
+langItem3.innerText = 'Italian';
+
+langList.appendChild(langItem3);
+
+document.body.appendChild(langList);
+
+
+
+// Datos de contacto - Footer
+var footer = document.createElement('footer')
+footer.innerText = 'Tlf: +34 000 00 00 - email: pinochoGD@ISDI.com'
+
+document.body.appendChild(footer)
