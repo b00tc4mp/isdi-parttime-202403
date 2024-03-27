d08198d1519bad51686fa44f4886e8c4448550e8
Eduardo Sanchez Criado <eduard.criado@gmail.com>
2024-03-27 19:32:33 +0100

    add index.css #89

--- staff/eduardo-sanchez/playground/cv/index.js
+++ staff/eduardo-sanchez/playground/cv/index.js
@@ -37,25 +37,80 @@ certificationsList.appendChild(certificationItem2)
 document.body.appendChild(certificationsList)
 
 var experienceTitle = document.createElement('h2')
-
 experienceTitle.innerText = 'Experience'
 
 document.body.appendChild(experienceTitle)
 
-var experienceList = document.createElement('ul')
+var experiencesList = document.createElement('ul')
 
 var experienceItem1 = document.createElement('li')
 experienceItem1.innerText = "Practicas en Google (Python department)"
 
-experienceList.appendChild(experienceItem1)
+experiencesList.appendChild(experienceItem1)
 
 var experienceItem2 = document.createElement('li')
 
 experienceItem2.innerText = "Profesor apoyo alumnos Bootcamp(ISDI Coders)"
 
-experienceItem1.appendChild(experienceItem2)
+experiencesList.appendChild(experienceItem2)
 
-document.body.appendChild(experienceList)
+var experienceItem3 = document.createElement('li')
+experienceItem3.innerText = " Software Developer (Microsoft, 2022)"
+
+experiencesList.appendChild(experienceItem3)
+
+document.body.appendChild(experiencesList)
+
+/*se puede resumir de esta forma para no tener q ir añadiendo cada elemento de la lista uno a uno**/
+
+
+// var headTitle = document.querySelector('title');
+// headTitle.innerText = 'CV - Eduardo Sanchez';
+
+// var title = document.createElement('h1');
+// title.innerText = headTitle.innerText;
+// document.body.appendChild(title);
+
+// // Datos para las certificaciones
+// var certificationsData = [
+//     'Responsive Web Design (FreeCodeCamp)',
+//     'Full-Stack Developer (ISDI Coders)'
+// ];
+
+// // Crear título para certificaciones
+// var certificationsTitle = document.createElement('h2');
+// certificationsTitle.innerText = "Certifications";
+// document.body.appendChild(certificationsTitle);
+
+// // Crear lista de certificaciones y agregar cada elemento
+// var certificationsList = document.createElement('ul');
+// for (var i = 0; i < certificationsData.length; i++) {
+//     var certificationItem = document.createElement('li');
+//     certificationItem.innerText = certificationsData[i];
+//     certificationsList.appendChild(certificationItem);
+// }
+// document.body.appendChild(certificationsList);
+
+// // Datos para la experiencia
+// var experienceData = [
+//     "Prácticas en Google (Python department)",
+//     "Profesor apoyo alumnos Bootcamp (ISDI Coders)",
+//     "Software Developer (Microsoft, 2022)"
+// ];
+
+// // Crear título para experiencia
+// var experienceTitle = document.createElement('h2');
+// experienceTitle.innerText = 'Experience';
+// document.body.appendChild(experienceTitle);
+
+// // Crear lista de experiencia y agregar cada elemento
+// var experienceList = document.createElement('ul');
+// for (var j = 0; j < experienceData.length; j++) {
+//     var experienceItem = document.createElement('li');
+//     experienceItem.innerText = experienceData[j];
+//     experienceList.appendChild(experienceItem);
+// }
+// document.body.appendChild(experienceList);
 
 
 
