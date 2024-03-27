de0ea0169153fdd55bec82cac4411b90d23e6d20
Adrian <adriangonzaloma@gmail.com>
2024-03-27 13:04:00 +0100

    update cv #64 WIP

--- staff/adrian-martin/playground/cv/index.js
+++ staff/adrian-martin/playground/cv/index.js
@@ -1,59 +1,45 @@
-var headTitle = document.querySelector('title')
-headTitle.innerText = 'CV - Adrian Martin Gonzalo'
+document.title = 'CV - Adrian Martin Gonzalo';
 
-var title = document.createElement('h1')
-title.innerText = headTitle.innerText
+var title = document.createElement('h1');
+title.innerText = 'CV - Adrian Martin Gonzalo';
+document.body.appendChild(title);
 
-document.body.appendChild(title)
+var introTitle = document.createElement('h2');
+introTitle.innerText = 'Intro';
+document.body.appendChild(introTitle);
 
+var introDescription = document.createElement('p');
+introDescription.innerText = 'Aquí puedes agregar una descripción sobre ti.';
+document.body.appendChild(introDescription);
 
-var introTitle = document.createElement('h2')
-introTitle.innerText = 'Intro'
+var certificationTitle = document.createElement('h2');
+certificationTitle.innerText = 'Certificaciones';
+document.body.appendChild(certificationTitle);
 
-document.body.appendChild(introTitle)
+var certificationList = document.createElement('ul');
 
+var certificationItem1 = document.createElement('li');
+certificationItem1.innerText = 'Preimpresión Digital';
+certificationList.appendChild(certificationItem1);
 
-var introDescription = document.createElement('p')
-introDescription.innerText = 'Nose que poner, pero me servira como ejemplo'
+var certificationItem2 = document.createElement('li');
+certificationItem2.innerText = 'Full Stack Develop ISDI Coders';
+certificationList.appendChild(certificationItem2);
 
-document.body.appendChild(introDescription)
+document.body.appendChild(certificationList);
 
+var experienceTitle = document.createElement('h2');
+experienceTitle.innerText = 'Experiencia';
+document.body.appendChild(experienceTitle);
 
-var certificationTitle = document.createElement('h2')
-certificationTitle.innerText = 'certification'
+var experiencesList = document.createElement('ul');
 
-document.body.appendChild(certificationTitle)
+var experienceItem1 = document.createElement('li');
+experienceItem1.innerText = 'Dosa - 6 meses';
+experiencesList.appendChild(experienceItem1);
 
+var experienceItem2 = document.createElement('li');
+experienceItem2.innerText = 'Camarero - 6 meses';
+experiencesList.appendChild(experienceItem2);
 
-var certificationList = document.createElement('ul')
-
-var certificationitem2 = document.createElement('li')
-certificationitem2.innerText = 'Educación Secundaria Obligatoria'
-
-certificationList.appendChild(certificationitem2)
-
-var certificationitem2 = document.createElement('li')
-certificationitem2.innerText = 'Preimpresión Digital'
-
-certificationList.appendChild(certificationitem2)
-
-document.body.appendChild(certificationList)
-
-var experienceTitle = document.createElement('h2')
-experienceTitle.innerText = 'Experience'
-
-document.body.appendChild(experienceTitle)
-
-var experiencesList = document.createElement('ul')
-
-var experienceItem1 = document.createElement('li')
-experienceItem1.innerText = 'Dosa 6 meses'
-
-document.body.appendChild(experienceItem1)
-
-var experienceItem2 = document.createElement('li')
-experienceItem2.innerText = 'Camarero 6 meses'
-
-document.body.appendChild(experienceItem2)
-
-document.body.appendChild(experiencesList)
+document.body.appendChild(experiencesList);
