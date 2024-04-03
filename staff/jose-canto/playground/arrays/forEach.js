4867b3e134a2e2e7faffc4e7d1ee07aa2b26b772
Jose A. Canto <jose.a.c.lopez@gmail.com>
2024-04-03 15:12:48 +0200

    add forEach methods #53

---
+++ staff/jose-canto/playground/arrays/forEach.js
@@ -0,0 +1,27 @@
+// Recrear metodo forEach
+
+function forEach(array, callback) {
+
+  for (let i = 0; i < array.length; i++) {
+
+    var element = array[i]
+
+    callback(element)
+  }
+}
+
+// Tenemos un array, llamamos a la función y le pasamos el array, y la función que queremos que se ejecute para cada elemento
+
+var chars = ['a', 'b', 'c'];
+forEach(chars, function (char) { console.log(char.toUpperCase()) })
+// Expected output: "A"
+// Expected output: "B"
+// Expected output: "C"
+
+
+var numbers = [10, 20, 30, 40,]
+forEach(numbers, function (num) { console.log(num * 10) })
+// Expected output: "100"
+// Expected output: "200"
+// Expected output: "300"
+// Expected output: "400"
