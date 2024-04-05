b0b52f805efb182b0a92a803c19477dc1cba04d6
Eduardo Sanchez Criado <eduard.criado@gmail.com>
2024-04-05 17:38:17 +0200

    add printDomTree folder & recursion examples within folder pruebas #89

---
+++ staff/eduardo-sanchez/playground/pruebasJs/recursion2.js
@@ -0,0 +1,44 @@
+/*
+function forEach(array, i, callback)  {
+    if (i < array.length) {
+        var element = array[i]
+
+        callback(element)
+    
+        forEach(array, i + 1, callback)
+    }
+}
+*/
+
+var nums = [10, 20, 30, 40, 50]
+//forEach(nums, 0, function(num) { console.log(num * 10) })
+nums.forEach(function (num) { console.log(num * 10) }) // Array.prototype.* methods
+
+var chars = ['a', 'b', 'c', 'd', 'e', 'f']
+//forEach(chars, 0, function(char) { console.log(char.toUpperCase()) })
+chars.forEach(function (char) { console.log(char.toUpperCase()) })
+
+var people = [{ name: 'Peter', age: 30 }, { name: 'Wendy', age: 29 }]
+//forEach(people, 0, function(person) { console.log(person.name.toUpperCase()) })
+people.forEach(function (person) { console.log(person.name.toUpperCase()) })
+
+var cart = [{ title: 'socks', quantity: 3, price: 10 }, { title: 't-shirt', quantity: 2, price: 20 }, { title: 'shoes', quantity: 1, price: 50 }]
+var total = 0
+//forEach(cart, 0, function(item) { total += item.quantity * item.price })
+cart.forEach(function (item) { total += item.quantity * item.price })
+console.log(total)
+
+// VM1335:17 100
+// VM1335:17 200
+// VM1335:17 300
+// VM1335:17 400
+// VM1335:17 500
+// VM1335:21 A
+// VM1335:21 B
+// VM1335:21 C
+// VM1335:21 D
+// VM1335:21 E
+// VM1335:21 F
+// VM1335:25 PETER
+// VM1335:25 WENDY
+// VM1335:32 120
---
+++ staff/eduardo-sanchez/playground/pruebasJs/recursion4.js
@@ -0,0 +1,40 @@
+function forEach(array, callback) {
+    (function loop(i) { // IIFE & closure
+        if (i < array.length) {
+            var element = array[i]
+
+            callback(element)
+
+            loop(i + 1)
+        }
+    })(0)
+}
+
+var nums = [10, 20, 30, 40, 50]
+forEach(nums, function (num) { console.log(num * 10) })
+
+var chars = ['a', 'b', 'c', 'd', 'e', 'f']
+forEach(chars, function (char) { console.log(char.toUpperCase()) })
+
+var people = [{ name: 'Peter', age: 30 }, { name: 'Wendy', age: 29 }]
+forEach(people, function (person) { console.log(person.name.toUpperCase()) })
+
+var cart = [{ title: 'socks', quantity: 3, price: 10 }, { title: 't-shirt', quantity: 2, price: 20 }, { title: 'shoes', quantity: 1, price: 50 }]
+var total = 0
+forEach(cart, function (item) { total += item.quantity * item.price })
+console.log(total)
+
+// VM1354:16 100
+// VM1354:16 200
+// VM1354:16 300
+// VM1354:16 400
+// VM1354:16 500
+// VM1354:19 A
+// VM1354:19 B
+// VM1354:19 C
+// VM1354:19 D
+// VM1354:19 E
+// VM1354:19 F
+// VM1354:22 PETER
+// VM1354:22 WENDY
+// VM1354:27 120
