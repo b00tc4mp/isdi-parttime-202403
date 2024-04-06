6cd4cb8e626dd862f6621c2d124b816acd2dd69f
Jose A. Canto <jose.a.c.lopez@gmail.com>
2024-04-06 12:31:32 +0200

    add method pop #53

---
+++ staff/jose-canto/playground/arrays/02_standalone/pop.demo_0.js
@@ -0,0 +1,46 @@
+//delete Array.prototype.pop // Elminamos el metodo pop para evitar usar el metodo.
+
+console.info(" --- CASE delete last element ---");
+
+function pop(array) {
+
+  var lastElement = array[array.length - 1]
+
+  array.length = array.length - 1
+
+  return lastElement
+}
+
+var plants = ['broccoli', 'cauliflower', 'cabbage', 'kale', 'tomato'];
+
+var elementDeleted = pop(plants);
+console.log(elementDeleted);
+//* Expected output: "tomato"
+
+console.log(plants);
+//* Expected output: Array ["broccoli", "cauliflower", "cabbage", "kale"]
+
+//! TEST ASSERT
+console.assert(elementDeleted === "tomato", "element deleted is tomato")
+console.assert(plants.length === 4, "length of plants is 4")
+console.assert(plants[0] === "broccoli", "first element is broccoli")
+console.assert(plants[1] === "cauliflower", "second element is not broccoli");
+console.assert(plants[2] === "cabbage", "third element is cabbage")
+console.assert(plants[3] === "kale", "four element is kale")
+
+//? ----------------------------------------------------------------
+
+var elementDeleted = pop(plants);
+console.log(elementDeleted)
+
+console.log(plants);
+//* Expected output: Array ["broccoli", "cauliflower", "cabbage"]
+
+//! TEST ASSERTs
+console.assert(elementDeleted === "kale", " element deleted is kale")
+console.assert(plants.length === 3, "length of plants is 3")
+console.assert(plants[0] === "broccoli", "first element is broccoli")
+console.assert(plants[1] === "cauliflower", "second element is not broccoli");
+console.assert(plants[2] === "cabbage", "third element is cabbage")
+
+
