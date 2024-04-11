d3b486df2916f289218b612bcd36aac2749e598b
jordi1094 <63929965+jordi1094@users.noreply.github.com>
2024-04-11 19:14:29 +0200

    upload custom and irriys #59

--- staff/jordi-isern/playground/arrays/native-method/Array.prototype.pop.demo.js
+++ staff/jordi-isern/playground/arrays/native-method/Array.prototype.pop.demo.js
@@ -1,12 +1,19 @@
 var plants = ['broccoli', 'cauliflower', 'cabbage', 'kale', 'tomato'];
 
-console.log(plants.pop());
-// Expected output: "tomato"
+var poped = plants.pop();
+console.assert(poped === 'tomato', "tiene que devolver tomato")
+console.assert(plants.length === 4, "el length tiene que ser 4")
 
-console.log(plants);
-// Expected output: Array ["broccoli", "cauliflower", "cabbage", "kale"]
-
+console.assert(plants[0] === 'broccoli', 'el elemento 0 ha de ser boccoli')
+console.assert(plants[1] === 'cauliflower', 'el elemento 1 tiene que ser cauliflower')
+console.assert(plants[2] === 'cabbage', 'el elemento 1 tiene que ser cabbage')
+console.assert(plants[3] === 'kale', 'el elemento 1 tiene que ser kale')
 plants.pop();
 
-console.log(plants);
-// Expected output: Array ["broccoli", "cauliflower", "cabbage"]
+console.assert(plants.length === 3, 'el largo tiene que ser 3')
+
+
+console.assert(plants[0] === 'broccoli', 'el elemento 0 ha de ser boccoli')
+console.assert(plants[1] === 'cauliflower', 'el elemento 1 tiene que ser cauliflower')
+console.assert(plants[2] === 'cabbage', 'el elemento 1 tiene que ser cabbage')
+
