7a1c7d82275a47d417350f67f59f88a3a3f8bf0e
Adrian <adriangonzaloma@gmail.com>
2024-04-26 00:11:45 +0200

    rework the register WIP #64

--- staff/adrian-martin/playground/shapes/gameGoolkeeper/logic.js
+++ staff/adrian-martin/playground/shapes/gameGoolkeeper/logic.js
@@ -13,38 +13,31 @@ var PASSWORD_REGEX = /^[a-zA-Z0-9-_]+$/
 
         // validar el email
         if(!EMAIL_REGEX.test(email))
-            throw new Error('email is not valid')
+            throw new ContentError('email is not valid')
 
         
         // validar el usuario
         if(!USERNAME_REGEX.test(username))
-            throw new Error('username is not valid')
+            throw new ContentError('username is not valid')
             
         
         // validar la constraseña
         if(!PASSWORD_REGEX.test(password))
-            throw new Error('password is not valid')
+            throw new ContentError('password is not valid')
 
 
         // revisar si la contraseña coincide con la passwordRepeat
         if(password !== passwordRepeat)
-            throw new Error('password don\'t match')
-        
+            throw new MatchError('password don\'t match')
 
-        // Si usersJson esta vacio devuelve array vacio
-        if(!usersJson) usersJson = '[]'
 
-        // Si hay usuario - convertir a array
-        var users = JSON.parse(usersJson)
-
-        // Revisar si ya existe y existe
-        var user = users.find(function(user) {
+        var user = data.findUser(function(user) {
             return user.email === email || user.username === username
         })
-
+        
         // Revisar si ya existe y NO existe
         if (user)
-            throw new Error('user already exists')
+            throw new DuplicityError('user already exists')
         
 
         // inyectar el usuario
@@ -54,13 +47,7 @@ var PASSWORD_REGEX = /^[a-zA-Z0-9-_]+$/
             password: password,
         }
 
-        // meter en el array
-        users.push(user)
+        data.insertUser(user)
 
-        // guardar en servidor
-        usersJson = JSON.stringify(users)
-
-        // guardar en local store
-        localStorage.users = usersJson
+        
     }
-
--- staff/adrian-martin/playground/shapes/gameGoolkeeper/register/index.js
+++ staff/adrian-martin/playground/shapes/gameGoolkeeper/register/index.js
@@ -17,85 +17,25 @@ registerForm.onSubmit(function(event) {
     var password = registerForm.getPassword()
     var passwordRepeat = registerForm.getRetypePassword()
 
-    var logic = {}
 
-    // MANEJAR DATOS/input validation
-//     logic.registerUser = function(email, username, password, passwordRepeat){
-//         // trae users, trae el json
-//         var usersJson = localStorage.users
+    try{
+        if(logic.registerUser(email, username, password, passwordRepeat))
 
-//         // validar el email
-//         if(!EMAIL_REGEX.test(email))
-//             throw new Error('email is not valid')
-
+            registerForm.clear()
+    }catch(error){
+        if (error instanceof ContentError) 
+        registerForm.setFeedback(error.message + '. please, repeat it')
         
-//         // validar el usuario
-//         if(!USERNAME_REGEX.test(username))
-//             throw new Error('username is not valid')
-            
-        
-//         // validar la constraseña
-//         if(!PASSWORD_REGEX.test(password))
-//             throw new Error('password is not valid')
+        if(error instanceof MatchError)
+        registerForm.setFeedback(error.message + '. please, repeat them')
 
+        else if(error instanceof DuplicityError)
+        registerForm.setFeedback(error.message + '. please, enter new one')
 
-//         // revisar si la contraseña coincide con la passwordRepeat
-//         if(password !== passwordRepeat)
-//             throw new Error('password don\'t match')
-        
-
-//         // Si usersJson esta vacio devuelve array vacio
-//         if(!usersJson) usersJson = '[]'
-
-//         // Si hay usuario - convertir a array
-//         var users = JSON.parse(usersJson)
-
-//         // Revisar si ya existe y existe
-//         var user = users.find(function(user) {
-//             return user.email === email || user.username === username
-//         })
-
-//         // Revisar si ya existe y NO existe
-//         if (user)
-//             throw new Error('user already exists')
-        
-
-//         // inyectar el usuario
-//         user ={
-//             email: email,
-//             username: username,
-//             password: password,
-//         }
-
-//         // meter en el array
-//         users.push(user)
-
-//         // guardar en servidor
-//         usersJson = JSON.stringify(users)
-
-//         // guardar en local store
-//         localStorage.users = usersJson
-//     }
-//     try{
-//         if(logic.registerUser(email, username, password, passwordRepeat))
-
-//             registerForm.clear()
-//     }catch(error){
-//         // alert(error.message)
-//         registerForm.setFeedback(error.message)
-//     }
-
-try{
-    if(logic.registerUser(email, username, password, passwordRepeat))
-
-        registerForm.clear()
-}catch(error){
-    // alert(error.message)
-    registerForm.setFeedback(error.message)
-}
-
+    else
+        registerForm.setFeedback('sorry, there was an error, please try again later')
+    }
 })
-
 
 var loginLink = new Link
 loginLink.setText('Login')
