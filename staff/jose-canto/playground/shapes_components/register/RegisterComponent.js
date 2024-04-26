75b3cb788489e2067ed55fb67584d1a99ccac7eb
Jose A. Canto <jose.a.c.lopez@gmail.com>
2024-04-26 07:50:15 +0200

    add separate the code, add try catch, add feedbackPanel and passwordField #53

--- staff/jose-canto/playground/shapes_components/register/RegisterComponent.js
+++ staff/jose-canto/playground/shapes_components/register/RegisterComponent.js
@@ -8,23 +8,26 @@ function RegisterComponent() {
   var emailField = new Field("email", "email", "E-mail")
   emailField.setPlaceholder("name@example.com")
 
-
   //* USERNAME
   var userField = new Field("user", "user", "Username")
   userField.setPlaceholder("Username")
 
-
   //* PASSWORD
-  var passwordField = new Field("password", "password", "Password")
+  var passwordField = new CheckPasswordField("password", "password", "Password")
   passwordField.setPlaceholder("Password")
 
   //* PASSWORD REPEAT
   var repeatPassword = new Field("passwordRepeat", "password", "Password Repeat")
   repeatPassword.setPlaceholder("Repeat Password")
 
-
-  //* Button
+  //* BUTTON
   var submitButton = new SubmitButton("Register")
+
+  //* FEEDBACK
+  var feedbackPanel = new Component("p")
+  feedbackPanel.addClass("Feedback")
+
+
 
   // Agregamos los elementos al formulario
   this.add(emailField)
@@ -32,6 +35,8 @@ function RegisterComponent() {
   this.add(passwordField)
   this.add(repeatPassword)
   this.add(submitButton)
+  this.add(feedbackPanel)
+
 }
 
 RegisterComponent.prototype = Object.create(Form.prototype)
@@ -57,4 +62,22 @@ RegisterComponent.prototype.getPasswordRepeat = function () {
   return repeatPasswordField.getValue()
 }
 
+RegisterComponent.prototype.setFeedback = function (message, clazz) {
+  var feedbackPanel = this.children[this.children.length - 1]
+
+  feedbackPanel.setText(message)
+  feedbackPanel.addClass(clazz)
+
+  setTimeout(function () {
+    feedbackPanel.setText("")
+  }, 2000)
+}
+
+RegisterComponent.prototype.clear = function () {
+  Form.prototype.clear.call(this)
+
+  var feedbackPanel = this.children[this.children.length - 1]
+
+  feedbackPanel.setText("")
+}
 
--- staff/jose-canto/playground/shapes_components/register/index.js
+++ staff/jose-canto/playground/shapes_components/register/index.js
@@ -5,20 +5,13 @@ view.addClass("View")
 // Creamos un encabezado con nivel 1 y establecemos el texto como "REGISTER"
 var headerRegister = new Header(1)
 headerRegister.setText("REGISTER")
-
 headerRegister.onClick(function (event) {
   event.preventDefault()
   alert("Click en el registro")
 })
 
-// Creamos un enlace para redirigir al usuario al inicio de sesión
-var loginLink = new Link()
-loginLink.setText("Login")
-
-
 // Creamos una instancia de la clase RegisterComponent
 var registerForm = new RegisterComponent()
-
 // Escuchamos el evento submit del formulario de registro
 registerForm.onSubmit(function (event) {
   event.preventDefault() // Prevenimos el comportamiento predeterminado del formulario
@@ -29,51 +22,20 @@ registerForm.onSubmit(function (event) {
   var password = registerForm.getPassword()
   var passwordRepeat = registerForm.getPasswordRepeat()
 
-  // Obtenemos los usuarios del Local Storage o creamos un array vacío si no existen
-  var usersJson = localStorage.users
-
-  if (!usersJson) {
-    usersJson = "[]"
-  }
-
-  // Convertimos la cadena JSON de usuarios a un array de objetos
-  var usersArray = JSON.parse(usersJson)
-
-  var userRegistered = usersArray.some(function (user) {
-    return user.email === email || user.username === username
-  })
-
-  if (userRegistered) {
-    alert("Users already exists ❌")
-
-  } else if (password !== passwordRepeat) {
-    alert(`Password don't match ❌`)
-
-  } else {
-    alert(`User ${username} created ✅`)
-
-    // Creamos un objeto con los datos del nuevo usuario
-    var user = {
-      email: email,
-      username: username,
-      password: password,
-      passwordRepeat: passwordRepeat
-    }
-    usersArray.push(user)
-    console.log(usersArray)
-
-    // Convertimos el array de usuarios de nuevo a una cadena JSON
-    var updatedUsersJson = JSON.stringify(usersArray)
-
-    // Guardamos la cadena JSON actualizada en el Local Storage
-    localStorage.users = updatedUsersJson
-    console.log(localStorage.users)
-
-    // Limpiamos el formulario de registro
+  try {
+    logic.registerUser(email, username, password, passwordRepeat)
     registerForm.clear()
-    //location.href = "../login" // Redireccionamos al usuario al inicio de sesión 
+    registerForm.setFeedback("User registered", "sucess")
+  } catch (error) {
+    //alert(error.message)
+    registerForm.setFeedback(error.message)
   }
 })
+
+// Creamos un enlace para redirigir al usuario al inicio de sesión
+var loginLink = new Link()
+loginLink.setText("Login")
+// loginLink.setTarget("_blank")
 
 // Configuramos un evento onclick para redirigir al usuario al inicio de sesión
 loginLink.onClick(function (event) {
