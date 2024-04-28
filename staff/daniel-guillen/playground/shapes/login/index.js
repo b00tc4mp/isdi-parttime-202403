5687c68607f8e35052fcfe6943a8069cf18b1166
danielguillen88 <danielguillen85@gmail.com>
2024-04-28 21:10:14 +0200

    fixed correct setting of login form #99

--- staff/daniel-guillen/playground/shapes/login/index.js
+++ staff/daniel-guillen/playground/shapes/login/index.js
@@ -1,13 +1,43 @@
 var view = new Component(document.body)
 view.addClass('View')
 
-var title = new Heading(1)
+var titleGame = new Heading(1)
+titleGame.setText('Encuentra el Punto Blanco')
+
+var title = new Heading(2)
 title.setText('Login')
 title.onClick(function () {
     alert('Al hacer clic en este título no obtendrás nada')
 })
 
 var loginForm = new LoginForm
+
+loginForm.onSubmit(function (event) {
+    event.preventDefault()
+
+    var username = loginForm.getUsername()
+    var password = loginForm.getPassword()
+
+    try {
+        logic.loginUser(username, password)
+
+        loginForm.clear()
+
+        loginForm.setFeedback('Se inicio sesion', 'success')
+
+        setTimeout(function () {
+            location.href = '../home'
+        }, 3000)
+    } catch (error) {
+        if (error instanceof ContentError)
+            loginForm.setFeedback(error.message + ', por favor, corrijalo')
+        else if (error instanceof MatchError)
+            loginForm.setFeedback('Por favor, corrijalo')
+        else
+            loginForm.setFeedback('Lo siento, hubo un error, inténtalo de nuevo más tarde')
+    }
+})
+
 var registerLink = new Link
 registerLink.setText('Register')
 //Forma de hacerlo sin complicarse la vida //registerLink.setUrl('../register')
@@ -19,6 +49,7 @@ registerLink.onClick(function (event) {
     }, 500)
 })
 
+view.add(titleGame)
 view.add(title)
 view.add(loginForm)
 view.add(registerLink)
