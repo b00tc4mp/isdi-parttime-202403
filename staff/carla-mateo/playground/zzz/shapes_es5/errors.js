2e31a43e88a84aaff6c34142e8c3c2a6a7d51ee8
Carla Mateo <carlazoom@gmail.com>
2024-04-30 17:33:23 +0200

    add folder shapes_es5 #69

---
+++ staff/carla-mateo/playground/zzz/shapes_es5/errors.js
@@ -0,0 +1,27 @@
+function ContentError(message) {
+    this.message = message
+
+    Error.captureStackTrace(this, this.constructor)
+}
+
+ContentError.prototype = Object.create(Error.prototype)
+ContentError.prototype.constructor = ContentError
+ContentError.prototype.name = ContentError.name
+
+function MatchError(message) {
+    this.message = message
+    Error.captureStackTrace(this, this.constructor)
+}
+
+MatchError.prototype = Object.create(Error.prototype)
+MatchError.prototype.constructor = MatchError
+MatchError.prototype.name = MatchError.name
+
+function DuplicityError(message) {
+    this.message = message
+    Error.captureStackTrace(this, this.constructor)
+}
+
+DuplicityError.prototype = Object.create(Error.prototype)
+DuplicityError.prototype.constructor = DuplicityError
+DuplicityError.prototype.name = DuplicityError.name
