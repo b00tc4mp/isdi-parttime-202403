dbde61081e634ed8f1916b7276e989ffbaacb845
Sandra Dominguez Sanchez <sandra.core.1989@hotmail.es>
2024-05-01 19:29:29 +0200

    I add new folder socialcode #126

---
+++ staff/sandra-dominguez/socialcode/app/errors.js
@@ -0,0 +1,23 @@
+class ContentError extends Error {
+    constructor(message) {
+        super(message)
+
+        this.name = this.constructor.name
+    }
+}
+
+class MatchError extends Error {
+    constructor(message) {
+        super(message)
+
+        this.name = this.constructor.name
+    }
+}
+
+class DuplicityError extends Error {
+    constructor(message) {
+        super(message)
+
+        this.name = this.constructor.name
+    }
+}
