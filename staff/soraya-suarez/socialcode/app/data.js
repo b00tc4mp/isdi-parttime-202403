14bc8477594c9fda75285599b809a78115dbf82f
Sor91 <soraya.sv91@gmail.com>
2024-05-04 21:21:54 +0100

    add initial code for social app #142

---
+++ staff/soraya-suarez/socialcode/app/data.js
@@ -0,0 +1,53 @@
+const data = {}
+
+data.findUser = callback => {
+    let usersJson = localStorage.users
+
+    if (!usersJson) usersJson = '[]'
+
+    const users = JSON.parse(usersJson)
+
+    const user = users.find(callback)
+
+    return user
+}
+
+data.insertUser = user => {
+    let usersJson = localStorage.users
+
+    if (!usersJson) usersJson = '[]'
+
+    const users = JSON.parse(usersJson)
+
+    users.push(user)
+
+    usersJson = JSON.stringify(users)
+
+    localStorage.users = usersJson
+}
+
+data.findPosts = callback => {
+    let postsJson = localStorage.posts
+
+    if (!postsJson) postsJson = '[]'
+
+    const posts = JSON.parse(postsJson)
+
+    const filtered = posts.filter(callback)
+
+    return filtered
+}
+
+data.insertPost = post => {
+    let postsJson = localStorage.posts
+
+    if (!postsJson) postsJson = '[]'
+
+    const posts = JSON.parse(postsJson)
+
+    posts.push(post)
+
+    postsJson = JSON.stringify(posts)
+
+    localStorage.posts = postsJson
+}
