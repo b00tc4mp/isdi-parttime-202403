10bd08f5eae34538838d004dfa95677f417a345c
Johnny Rojas Cifra <157323400+Johnnyrc26@users.noreply.github.com>
2024-05-07 09:43:20 +0200

    create socialcode app with updates to components, logic and data folders #150

---
+++ staff/johnny-rojas/socialcode/app/errors.js
@@ -0,0 +1,24 @@
+class ContentError extends Error {
+  constructor(message) {
+      super(message)
+
+      //this.name = ContentError.name
+      this.name = this.constructor.name
+  }
+}
+
+class MatchError extends Error {
+  constructor(message) {
+      super(message)
+
+      this.name = this.constructor.name
+  }
+}
+
+class DuplicityError extends Error {
+  constructor(message) {
+      super(message)
+
+      this.name = this.constructor.name
+  }
+}
---
+++ staff/johnny-rojas/socialcode/app/home/components/Post.js
@@ -0,0 +1,42 @@
+class Post extends Component {
+  constructor(post) {
+      super('article')
+
+      const authorTitle = new Component('p')
+      authorTitle.setText(post.author)
+
+      const postTitle = new Component('h2')
+      postTitle.setText(post.title)
+
+      const postImage = new Image
+      postImage.setUrl(post.image)
+
+      const postDescription = new Component('p')
+      postDescription.setText(post.description)
+
+      const postDate = new Component('time')
+      postDate.setText(post.date)
+
+      this.add(authorTitle)
+      this.add(postTitle)
+      this.add(postImage)
+      this.add(postDescription)
+      this.add(postDate)
+
+      if (post.author === logic.getLoggedInUsername()) {
+          const deleteButton = new Button('Delete')
+
+          deleteButton.onClick(() => {
+              logic.deletePost(post.id)
+
+              this.onPostDeletedListener()
+          })
+
+          this.add(deleteButton)
+      }
+  }
+
+  onPostDeleted(listener) {
+      this.onPostDeletedListener = listener
+  }
+}
