4f3df3d9b10d9105cd7c648a46239df993cd6ebb
AgusBirman <agusbirman@gmail.com>
2024-05-08 15:45:51 +0200

    update code and fixed problems

--- staff/agustin-birman/playground/aleman/beginner-page/verbs/index.js
+++ staff/agustin-birman/playground/aleman/beginner-page/verbs/index.js
@@ -41,7 +41,7 @@ function showVerb() {
 
 document.addEventListener('DOMContentLoaded', showVerb);
 
-let checkButton = document.getElementById('checkButton')
+let checkButton = document.getElementById('checkButton-verb')
 
 checkButton.addEventListener('click', function () {
 
@@ -53,7 +53,7 @@ checkButton.addEventListener('click', function () {
         errorVerb.innerText = ''
         acumulationError = 0
 
-        const errorButtons = document.querySelectorAll('.errorButton');
+        const errorButtons = document.querySelectorAll('.errorButtonVerb');
         errorButtons.forEach(button => button.remove());
 
     } else {
@@ -62,13 +62,13 @@ checkButton.addEventListener('click', function () {
 
         if (acumulationError === 2) {
             const skipButton = document.createElement('button')
-            skipButton.classList.add('errorButton')
+            skipButton.classList.add('errorButtonVerb')
             skipButton.innerText = 'Saltar'
             skipButton.addEventListener('click', showVerb)
 
 
             const hintButton = document.createElement('button')
-            hintButton.classList.add('errorButton')
+            hintButton.classList.add('errorButtonVerb')
             hintButton.innerText = 'Ayuda'
 
             document.body.appendChild(skipButton)
@@ -78,6 +78,83 @@ checkButton.addEventListener('click', function () {
 
     }
 })
+
+// Array de verbos en español (Dativo)
+const verbosEspanol = ["agradecer", "ayudar", "confiar", "decir", "mostar", "explicar", "gustar", "indicar", "mandar", "mostrar", "ofrecer", "parecer", "pedir", "prestar", "prometer", "regalar", "recordar", "recomendar", "responder", "rogar", "seguir", "servir", "sugerir", "telefonar", "agradar", "interesar", "molestar", "importar", "faltar", "sobrar", "bastar", "quedar", "convenir", "corresponder", "dedicar", "destinar", "satisfacer", "encargar", "designar", "comunicar", "dar", "devolver", "entregar", "enviar", "prestar", "deber", "costar", "venir", "perdonar", "vender"];
+
+// Array de verbos en alemán (Dativo)
+const verbosAleman = ["danken", "helfen", "vertrauen", "sagen", "zeigen", "erklären", "gefallen", "anzeigen", "schicken", "zeigen", "anbieten", "scheinen", "bitten", "leihen", "versprechen", "schenken", "erinnern", "empfehlen", "antworten", "bitten", "folgen", "dienen", "vorschlagen", "anrufen", "gefallen", "interessieren", "stören", "wichtig sein", "fehlen", "übrig sein", "genügen", "bleiben", "passen", "entsprechen", "widmen", "bestimmen", "zufriedenstellen", "auftragen", "bestimmen", "mitteilen", "geben", "zurückgeben", "übergeben", "schicken", "leihen", "müssen", "kosten", "kommen", "verzeihen", "verkaufen"];
+
+// Array de verbos en español (Akkusativ)
+const verbosEspanolAkk = ["buscar", "encontrar", "ver", "mirar", "leer", "escribir", "comprar", "vender", "tomar", "beber", "comer", "preparar", "cocinar", "llevar", "traer", "pedir", "hacer", "crear", "necesitar", "querer", "amar", "odiar", "sentir", "escuchar", "entender", "preguntar", "invitar", "esperar", "llamar", "aceptar", "rechazar", "buscar", "encontrar", "conocer", "presentar", "entregar", "abrir", "cerrar", "romper", "cambiar", "empezar", "terminar", "olvidar", "recordar", "dejar", "ayudar", "intentar", "lograr", "aprender", "enseñar"];
+
+// Array de verbos en alemán (Akkusativ)
+const verbosAlemanAkk = ["suchen", "finden", "sehen", "schauen", "lesen", "schreiben", "kaufen", "verkaufen", "nehmen", "trinken", "essen", "zubereiten", "kochen", "tragen", "bringen", "bestellen", "machen", "schaffen", "brauchen", "wollen", "lieben", "hassen", "fühlen", "hören", "verstehen", "fragen", "einladen", "erwarten", "anrufen", "akzeptieren", "ablehnen", "suchen", "finden", "kennenlernen", "vorstellen", "übergeben", "öffnen", "schließen", "zerbrechen", "wechseln", "beginnen", "beenden", "vergessen", "erinnern", "lassen", "helfen", "versuchen", "erreichen", "lernen", "lehren"];
+
+const completeVerbs = verbosEspanol.concat(verbosEspanolAkk)
+const completeGermanVerbs = verbosAleman.concat(verbosAlemanAkk)
+
+let dativOrAkk;
+let inputDatAkk = document.querySelector('.dativ-akk')
+let inputVerbDativAkk = document.querySelector('.verb-dativ-akk')
+let errorDativAkk = document.getElementById('error-dativ-akk')
+let acumulationDatAkkError = 0
+
+if (completeGermanVerbs.length <= 49) {
+    dativOrAkk = 'D'
+} else {
+    dativOrAkk = 'A'
+}
+
+document.addEventListener('DOMContentLoaded', showDativAkkVerb)
+
+function showDativAkkVerb() {
+    indexDativAkk = Math.floor(Math.random() * completeGermanVerbs.length)
+    const showedVerb = completeGermanVerbs[indexDativAkk]
+    const elementVerb = document.getElementById('showDativAkk')
+    elementVerb.innerText = showedVerb
+    inputDatAkk.value = ''
+    inputVerbDativAkk.value = ''
+}
+
+let checkButtonDativ = document.getElementById('checkButton-dativAkk')
+
+checkButtonDativ.addEventListener('click', function () {
+
+    let inputDatAkkValue = inputDatAkk.value
+    let inputVerbDativAkkValue = inputVerbDativAkk.value
+    let checkDatAkkValue = completeVerbs[indexDativAkk]
+    if (inputVerbDativAkkValue === checkDatAkkValue && inputDatAkkValue === dativOrAkk) {
+        showDativAkkVerb()
+        errorDativAkk.innerText = ''
+        acumulationDatAkkError = 0
+
+        const errorDativAkkButtons = document.querySelectorAll('.errorDativAkk')
+        errorDativAkkButtons.forEach(button => button.remove());
+    } else {
+        errorDativAkk.innerText = 'Ups! no es verbo correcto'
+        acumulationDatAkkError++
+
+        if (acumulationDatAkkError === 2) {
+            const skipButton = document.createElement('button')
+            skipButton.classList.add('errorButtonDatAkk')
+            skipButton.innerText = 'Saltar'
+            skipButton.addEventListener('click', showVerb)
+
+
+            const hintButton = document.createElement('button')
+            hintButton.classList.add('errorButtonDatAkk')
+            hintButton.innerText = 'Ayuda'
+
+            document.body.appendChild(skipButton)
+            document.body.appendChild(hintButton)
+            inputDatAkk.innerText = ''
+            inputVerbDativAkk.innerText = ''
+        }
+    }
+})
+
+
 
 
 
