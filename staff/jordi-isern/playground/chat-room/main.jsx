1c4bb29f77ed9ecf600876acdf11b44c546a710d
jordi1094 <63929965+jordi1094@users.noreply.github.com>
2024-05-12 17:42:59 +0200

    Actualizar segun classe viernes 10/05 #59

--- staff/jordi-isern/playground/chat-room/main.jsx
+++ staff/jordi-isern/playground/chat-room/main.jsx
@@ -4,38 +4,6 @@ const root = ReactDOM.createRoot(rootElement)
 
 const title = <h1>Chat room</h1>
 
-const messages = [
-    {
-    username : 'pepito',
-    text: 'blah',
-    date: new Date().toISOString()
-    },
-    {
-        username : 'peterpan',
-        text: 'blah blah',
-        date: new Date().toISOString()
-    },
-    {
-        username : 'wendy',
-        text: 'bloooh',
-        date: new Date().toISOString()
-    }
-]
-function MessageList(props){
-    const messages = props.messages
-    const lis = messages.map(message => <li>
-        <h3>{message.username}</h3>
-        <p>{message.text}</p>
-        <time>{message.date}</time>
-    </li>)
-
-
-    const messageList = <ul>{lis}</ul>
-    
-    return messageList
-}
-
-root.render([title, <MessageList messages = {messages} />])
-
+root.render ([title, <Counter />, <Messages />])
 
  //TODO leer sobre bind() jason arnold  dan abramov
