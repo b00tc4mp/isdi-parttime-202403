678d23ad0031a450fe2fcd8a2c7b09641e3340b8
Jose A. Canto <jose.a.c.lopez@gmail.com>
2024-05-15 12:53:07 +0200

    add web scrapper #53

---
+++ staff/jose-canto/playground/web-scrapper/google.js
@@ -0,0 +1,9 @@
+var xhr = new XMLHttpRequest
+
+xhr.addEventListener('load', function () {
+  console.log(xhr.response)
+})
+
+xhr.open('GET', 'https://www.google.com/search?q=hello+world')
+
+xhr.send()
