010be076c6110180c7b8d3574345440e0e842bf4
Carla Mateo <carlazoom@gmail.com>
2024-05-15 17:34:23 +0200

    add logs in react component  #69

--- staff/carla-mateo/playground/chat-room/components/MessageList.jsx
+++ staff/carla-mateo/playground/chat-room/components/MessageList.jsx
@@ -1,12 +1,41 @@
-function MessageList({ title, messages }) {
-    return <section className="MessageList">
-        <h2 className="title">{title}</h2>
+const { Component } = React
 
-        <ul>{messages.map(message => <li>
-            <h3>{message.username}</h3>
-            <p>{message.text}</p>
-            <time>{message.date}</time>
-        </li>)}</ul>
+class MessageList extends Component {
+    constructor() {
+        super()
 
-    </section>
+        console.log('MessageList -> constructor')
+    }
+
+    componentDidMount() { // method overriding
+        console.log('MessageList -> componentDidMount')
+    }
+
+    componentDidUpdate() { // method overriding
+        console.log('MessageList -> componentDidUpdate')
+    }
+
+    componentWillUnmount() { // method overriding
+        console.log('MessageList -> componentWillUnmount')
+    }
+
+    componentWillReceiveProps(newProps, oldProps) { // method overriding
+        console.log('MessageList -> componentWillReceiveProps')
+    }
+
+    render() {
+        console.log('MessageList -> render')
+
+        const { title, messages } = this.props
+
+        return <section className="MessageList">
+            <h2 className="title">{title}</h2>
+
+            <ul>{messages.map(message => <li>
+                <h3>{message.username}</h3>
+                <p>{message.text}</p>
+                <time>{message.date}</time>
+            </li>)}</ul>
+        </section>
+    }
 }
