a5ff38febbc5e77f3d1f7c093162a1edefdb7605
danielhernandez <tarnegolet@hotmail.com>
2024-05-16 14:00:07 -0600

    implemented cli interaction and the ability to write a formatted MD table with the scraped content to the simple google scraper ~ task #56

--- staff/daniel-hernandez/playground/web-scraper/google.js
+++
@@ -1,9 +0,0 @@
-var xhr = new XMLHttpRequest();
-
-xhr.addEventListener("load", function () {
-  console.log(xhr.response());
-});
-
-xhr.open("GET", "https://www.google.com/search?q=hello+world");
-
-xhr.send();
