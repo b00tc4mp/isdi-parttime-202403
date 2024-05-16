808179b9951a1fe5296dcc34507be2e10732e50a
danielguillen88 <danielguillen85@gmail.com>
2024-05-16 20:13:58 +0200

    deleting folders created by mistake #162

--- staff/daniel-guillen/playground/chat-room/main.jsx
+++
@@ -1,5 +0,0 @@
-const root = ReactDOM.createRoot(document.getElementById('root'))
-
-const title = <h1>Counter & Chat Room</h1>
-
-root.render([title, <Counter />, <Messages />])
--- staff/daniel-guillen/playground/playing-shapes/shapes/components/core/Heading.js
+++
@@ -1,6 +0,0 @@
-function Heading(level) {
-    Component.call(this, 'h' + level)
-}
-
-Heading.prototype = Object.create(Component.prototype)
-Heading.prototype.constructor = Heading
