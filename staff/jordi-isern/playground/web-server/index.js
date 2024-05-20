5c036bbccbd3bccbe3b8a6d6957fe999c4d56b4a
jordi1094 <63929965+jordi1094@users.noreply.github.com>
2024-05-20 21:17:53 +0200

    apdate code to the class and read html in the servers, WIP see images #59!
    git add staff/jordi-isern/playground/web-server

--- staff/jordi-isern/playground/web-server/index.js
+++ staff/jordi-isern/playground/web-server/index.js
@@ -1,8 +1,48 @@
 const express = require('express')
+const fs = require('fs')
+const path = require('path')
 
 const server = express()
 
-server.get('/hello', (req, res) => {
+server.get('/*', (req, res) => {
+    const path = req.params[0]
+    const route = `./public/${path}`
+
+    fs.stat(route, (error, stats) =>{
+        if(error){
+            res.status(404).send(error.message)
+            
+            return
+        }
+        if(stats.isFile()){
+            fs.readFile(route, 'utf8', (error , content) => {
+
+                //se tiene que hacer ocn static 
+                if(error) {
+                    res.status(404).send(error.message)
+
+                    return
+                }
+                res.send(content)
+            })
+
+        } if (stats.isDirectory()){
+            fs.readdir(route, (error, files) => {
+                if(error){
+                    res.status(404).send(error.message)
+
+                    return
+                }
+                const html = `<ul>
+                    ${files.map(file = `<li>
+                        <a href = "${path}/${file}">${file}</a>
+                        </li>`).join('')}
+                </ul>`
+
+                res.send(html)
+            })
+        }
+    })
 
 })
 
--- staff/jordi-isern/playground/web-server/utils/utils.js
+++
@@ -1,3 +0,0 @@
-function toRandomCase(string){
-    return string.spliot('').map(char => Math.random() > 0.5? char.toUpperCase() : char.toLoweCase()).join('')
-}
