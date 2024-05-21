224ace1425d3b1b28f1bdd423cf9bfd662659327
danielguillen88 <danielguillen85@gmail.com>
2024-05-21 18:42:33 +0200

    search data on web server #99

--- staff/daniel-guillen/playground/web-server/index.js
+++ staff/daniel-guillen/playground/web-server/index.js
@@ -3,8 +3,28 @@ const express = require('express')
 const server = express()
 
 server.get('/hello', (req, res) => {
-    res.send('hello world')
+    res.send('hello, world!')   
 
+})
+server.get('/hello/:to', (req, res) => {
+    const { to } = req.params
+
+    res.send(`hello, ${to}!`)  
+
+})
+
+const data = [
+    { name: 'Peter Pan', age: 20 },
+    { name: 'Wendy Darling', age: 21 },
+    { name: 'Campanilla', age: 60 }
+]
+
+server.get('/search', (req, res) => {
+    const { q } = req.query
+
+    const filtered = data.filter(person => person.name.includes(q))
+
+    res.json(filtered)
 })
 
 server.listen(8080, () => console.log('server up'))
