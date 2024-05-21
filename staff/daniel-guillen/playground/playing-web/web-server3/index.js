8d1cd2b040cd33abd4af34bd4db0f219a0400950
danielguillen88 <danielguillen85@gmail.com>
2024-05-21 21:29:50 +0200

    add mime types to content types returned from server #99

---
+++ staff/daniel-guillen/playground/playing-web/web-server3/index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const fs = require('fs')
+
+const server = express()
+
+server.get('/*', (req, res) => {
+    const path = req.params[0]
+    const route = `./public/${path}`
+
+    fs.stat(route, (error, stats) => {
+        if (error) {
+            res.status(404).send(error.message)
+
+            return
+        }
+
+        if (stats.isFile())
+            fs.readFile(route, 'utf8', (error, content) => {
+                if (error) {
+                    res.status(404).send(error.message)
+
+                    return
+                }
+
+                res.send(content)
+            })
+        else if (stats.isDirectory())
+            fs.readdir(route, (error, files) => {
+                if (error) {
+                    res.status(404).send(error.message)
+
+                    return
+                }
+
+                const html = `<ul>
+                    ${files.map(file => `<li>
+                        <a href="${path}/${file}">${file}</a>
+                    </li>`).join('')}
+                </ul>`
+
+                res.send(html)
+            })
+    })
+})
+
+server.listen(8080, () => console.log('server up'))
--- staff/daniel-guillen/playground/web-server/index.js
+++ staff/daniel-guillen/playground/web-server/index.js
@@ -14,17 +14,42 @@ server.get('/*', (req, res) => {
             return
         }
 
-        if (stats.isFile())
-            fs.readFile(route, 'utf8', (error, content) => {
-                if (error) {
-                    res.status(404).send(error.message)
+        if (stats.isFile()) {
+            if (path.endsWith('.txt') || path.endsWith('.html') || path.endsWith('.css'))
+                fs.readFile(route, 'utf8', (error, content) => {
+                    if (error) {
+                        res.status(404).send(error.message)
 
-                    return
-                }
+                        return
+                    }
 
-                res.send(content)
-            })
-        else if (stats.isDirectory())
+                    if (path.endsWith('.txt'))
+                        res.setHeader('Content-Type', 'text/plain')
+                    else if (path.endsWith('.html'))
+                        res.setHeader('Content-Type', 'text/html')
+                    else if (path.endsWith('.css'))
+                        res.setHeader('Content-Type', 'text/css')
+
+                    res.send(content)
+                })
+            else
+                fs.readFile(route, (error, content) => {
+                    if (error) {
+                        res.status(404).send(error.message)
+
+                        return
+                    }
+
+                    if (path.endsWith('.ico'))
+                        res.setHeader('Content-Type', 'image/vnd.microsoft.icon')
+                    else if (path.endsWith('.png'))
+                        res.setHeader('Content-Type', 'image/png')
+                    else if (path.endsWith('.jpg'))
+                        res.setHeader('Content-Type', 'image/jpeg')
+
+                    res.send(content)
+                })
+        } else if (stats.isDirectory())
             fs.readdir(route, (error, files) => {
                 if (error) {
                     res.status(404).send(error.message)
