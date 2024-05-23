a9d7e070a3f66770f0bd6c0af62c30acd75a8b43
Debi312 <deboragarciaescudero@gmail.com>
2024-05-23 17:26:29 +0200

    add mime types to content types returned from server; create middleware file  #112

--- staff/debora-garcia/playground/web-server/index.js
+++ staff/debora-garcia/playground/web-server/index.js
@@ -1,62 +1,11 @@
 const express = require("express")
+const filesMiddleware=require("./filesMiddleware")
 
-//herramienta de node para leer archivos
-const fs = require("fs")
+
 
 const server = express()
 
-//leemos un directorio
-server.get("/*", (req, res) => {
-    const path = req.params[0]
-    const route = `./public/${path}`
-    // comprobamos si es fichero o directorio
-    fs.stat(route, (error, stats) => {
-
-        if (error) {
-            res.status(404).send(error.message)
-
-            return
-        }
-
-        if (stats.isFile()) {
-            fs.readFile(route, "utf8", (error, content) => {
-                if (error) {
-                    res.status(404).send(error.message)
-
-                    return
-                }
-                res.send(content)
-            })
-        } else if (stats.isDirectory()) {
-            //lista los ficheros que hay en una carpeta 
-            fs.readdir(route, (error, files) => {
-
-                if (error) {
-                    res.status(404).send(error.message)
-
-                    return
-                }
-                //devolvemos html:
-
-                /* const html = `<ul>
-                ${files.map(file => `<li>${file}</li>`).join("\n")}
-                </ul>`
-
-                res.send(html) */
-
-
-                const html =
-                    `<ul>
-                         ${files.map(file =>
-                        `<li>
-                        <a href="${path}/${file}">${file}</a>
-                         </li>`).join("\n")}
-                     </ul>`
-
-                res.send(html)
-            })
-        }
-    })
-})
+// separamos el middleware en otro fichero
+server.get("/*", filesMiddleware)
 
 server.listen(8080, () => console.log("server up"))
