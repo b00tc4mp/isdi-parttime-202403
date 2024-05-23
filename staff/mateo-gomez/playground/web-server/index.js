e1b9ae13018ac83b9e361e2306a101ce8736c4e1
unknown <Mateo Gmez-Luca>
2024-05-23 12:03:38 +0200

    add mine types to content types returned from server; move middleware to file; #98

--- staff/mateo-gomez/playground/web-server/index.js
+++ staff/mateo-gomez/playground/web-server/index.js
@@ -1,51 +1,11 @@
 const express = require('express')
-const fs = require('fs')
+const filesMiddleware = require('./filesMiddleware')
 
 
 const server = express()
 
-server.get('/*', (req, res) => {
-    //res.send(req.params[0]) 
-    const path = req.params[0]
-    const route = `./public/${path}`
+//server.use(express.static('public'))
 
-    fs.stat(route, (error, stats) => {
-
-        if (error) {
-            res.status(404).send(error.message)
-
-            return
-        }
-
-        if (stats.isFile())
-
-            fs.readFile(route, 'utf8', (error, content) => {
-                if (error) {
-                    res.status(404).send(error.message)
-
-                    return
-                }
-
-                res.send(content)
-            })
-        else if (stats.isDirectory())
-            fs.readdir(route, (error, files) => {
-                if (error) {
-                    res.status(404).send(error.message)
-
-                    return
-                }
-                const html = `<ul>
-                ${files.map(file => `<li>
-                <a href = "${path}/${file}">${file} </a>
-                </li>`).join('')}
-                </ul>`
-
-                res.send(html)
-            })
-    })
-
-
-})
+server.get('/*', filesMiddleware)
 
 server.listen(8080, () => console.log('server up'))
