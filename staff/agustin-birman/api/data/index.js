fa3d312282ae9de0ac08fcd26c8ee2c750dbe111
AgusBirman <agusbirman@gmail.com>
2024-05-24 19:08:27 +0200

    add api logic #153

---
+++ staff/agustin-birman/api/data/index.js
@@ -0,0 +1,8 @@
+import fs from 'fs'
+import { SystemError } from '../errors'
+
+const data = {}
+
+data.findUser = (condition, callback) => {
+
+}
---
+++ staff/agustin-birman/api/index.js
@@ -0,0 +1,94 @@
+import { json } from 'body-parser'
+import { isUtf8 } from 'buffer'
+import express from 'express'
+import fs from 'fs'
+
+const api = express()
+
+const jsonBodyParser = express.json({ strict: true, type: 'application/json' })
+
+api.get('/posts', (req, res) => {
+    fs.readFile('.data/posts.json', 'utf8', (error, json) => {
+        if (error) {
+            res.status(500).json({ error: error.constructor.name, message: error.message })
+
+            return
+        }
+
+        const users = JSON.parse(json)
+        res.json(users)
+    })
+})
+
+api.get('/users', (req, res => {
+    fs.readFile('./data/users.json', 'utf8', (error, json) => {
+        if (error) {
+            res.status(500).json({ error: error.constructor.name, message: error.message })
+
+            return
+        }
+
+        const users = JSON.parse(json)
+        res.json(users)
+    })
+}))
+
+
+api.post('/users', jsonBodyParser, (req, res) => {
+    const user = req.body
+
+    fs.readFile('./data/users.json', 'utf8', (error, json) => {
+        if (error) {
+            res.status(500).json({ error: error.constructor.name, message: error.message })
+
+            return
+        }
+
+        const users = JSON.parse(json)
+        users.push(user)
+
+        const newJson = JSON.stringify(users)
+
+        fs.writeFile('./data/users.json', newJson, error => {
+            if (error) {
+                res.status(500).json({ error: error.constructor.name, message: error.message })
+
+                return
+            }
+            res.status(201).send()
+        })
+    })
+})
+
+api.post('/posts', jsonBodyParser, (req, res) => {
+    const post = req.body
+
+    fs.readFile('./data/posts.json', 'utf8', (error, json) => {
+        if (error) {
+            res.status(500).json({ error: error.constructor.name, message: error.message })
+
+            return
+        }
+
+        const posts = JSON.parse(json)
+
+        post.id = `${Math.random().toString().slice(2)}-${Date.now()}`
+        post.date = new Date().toISOString()
+
+        posts.push(post)
+
+        const newJson = JSON.stringify(posts)
+
+        fs.writeFile('./data/posts.json', newJson, error => {
+            if (error) {
+                res.status(500).json({ error: error.constructor.name, message: error.message })
+
+                return
+            }
+
+            res.status(201).send()
+        })
+    })
+})
+
+api.listen(8080, () => console.log('api is up'))
