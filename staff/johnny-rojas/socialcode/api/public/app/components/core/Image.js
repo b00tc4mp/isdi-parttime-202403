9e1a3453287eed81ff7ec7ea2ea48b90b4275f1c
Johnny Rojas Cifra <157323400+Johnnyrc26@users.noreply.github.com>
2024-05-30 17:44:06 +0200

    creation of the API folder and modification of the data and logic folders to work with a server #150

---
+++ staff/johnny-rojas/socialcode/api/public/app/components/core/Image.js
@@ -0,0 +1,15 @@
+class Image extends Component {
+  constructor() {
+    super('img')
+
+    this.addClass('Image')
+  }
+
+  setUrl(url) {
+    this.container.src = url
+  }
+
+  setAltText(altText) {
+    this.container.alt = altText
+  }
+}
