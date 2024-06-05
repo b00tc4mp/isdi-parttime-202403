56ec5e40c1ebc649dcf153b6ec51c26c407a65ec
Jose A. Canto <jose.a.c.lopez@gmail.com>
2024-06-05 07:35:12 +0200

    add cors at app #131

---
+++ staff/jose-canto/socialcode/api/cors.js
@@ -0,0 +1,8 @@
+
+export default api.use("*", (req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Methods", "*")
+  res.setHeader("Access-Control-Allow-Headers", "*")
+
+  next()
+})
--- staff/jose-canto/socialcode/api/index.js
+++ staff/jose-canto/socialcode/api/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
-//import fs from "fs" 
+// import cors from "./cors.js"
+import cors from "cors"
 
 import logic from "./logic/index.js"
 
@@ -7,27 +8,18 @@ const api = express()
 
 api.use(express.static("public"))
 
+api.use(cors())
+//api.use("*", cors)
+
+
 const jsonBodyParser = express.json({ strict: true, type: "application/json" })
 
 api.get("/", (req, res) => {
   res.send("Hello World")
 })
 
-
 api.get('/posts', (req, res) => {
-  // fs.readFile("./data/posts.json", "utf-8", (error, data) => {
-  //   if (error) {
-  //     res.status(500).json({ error: error.constructor.name, message: error.message })
-  //     return
-  //   }
 
-  //   // res.setHeader("Content-Type", "application/json") otra opcion de enviar un header
-  //   // res.send(data) otra opcion de enviar la data
-
-  //   const posts = JSON.parse(data)
-
-  //   res.json(posts)
-  // })
   try {
     logic.getAllPosts((error, posts) => {
       if (error) {
@@ -43,75 +35,11 @@ api.get('/posts', (req, res) => {
   }
 })
 
-
-// este endpoint no es necesario ya que obtenemos a todos los usuarios
-// api.get('/users', (req, res) => {
-//   fs.readFile("./data/users.json", "utf-8", (error, data) => {
-//     if (error) {
-//       res.status(500).json({ error: error.constructor.name, message: error.message })
-//       return
-//     }
-
-//     // res.setHeader("Content-Type", "application/json") otra opcion de enviar un header
-//     // res.send(data) otra opcion de enviar la data
-
-//     const users = JSON.parse(data)
-
-//     res.json(users)
-//   })
-// })
-
-
-// function jsonBodyParser(req, res, next) {  En vez de montar nosotros el jsonBodyParser, lo utilizamos de forma nativa de express
-//   const contentType = req.headers["content-type"]
-
-//   if (contentType.includes("application/json")) {
-//     let data = ""
-
-//     req.on("data", chunk => {
-//       data += chunk
-//     })
-
-//     req.on("end", () => {
-//       const body = JSON.parse(data)
-
-//       req.body = body
-//       next()
-
-//     })
-//   } else {
-//     next()
-//   }
-// }
-
-
 api.post("/users", jsonBodyParser, (req, res) => {
 
-  //const user = req.body
-
-  // fs.readFile("./data/users.json", "utf-8", (error, existingData) => {
-  //   if (error) {
-  //     res.status(500).json({ error: error.constructor.name, message: error.message })
-  //     return
-  //   }
-
-  //   const users = JSON.parse(existingData)
-  //   users.push(user)
-
-  //   const usersJson = JSON.stringify(users)
-
-  //   fs.writeFile("./data/users.json", usersJson, error => {
-  //     if (error) {
-  //       res.status(500).json({ error: error.constructor.name, message: error.message })
-  //       return
-  //     }
-  //     res.status(201).send(user)
-  //   })
-  // })
-
-  const { username, password } = req.body
+  const { name, surname, email, username, password, passwordRepeat } = req.body
   try {
-    logic.registerUser(username, password, (error) => {
+    logic.registerUser(name, surname, email, username, password, passwordRepeat, (error) => {
 
       if (error) {
         res.status(500).json({ error: error.constructor.name, message: error.message })
@@ -143,9 +71,9 @@ api.post("/users/auth", jsonBodyParser, (req, res) => {
         return
       }
 
+
       res.send()
       console.log(`User ${username} authenticated`)
-      //console.log("User " + username + " authenticated")
 
     })
 
@@ -171,6 +99,7 @@ api.get("/users/:targetUsername", (req, res) => {
       res.json(name)
     })
 
+
   } catch (error) {
 
     res.status(500).json({ error: error.constructor.name, message: error.message })
@@ -178,38 +107,16 @@ api.get("/users/:targetUsername", (req, res) => {
   }
 })
 
+
+
 api.post("/posts", jsonBodyParser, (req, res) => {
-
-  // const post = req.body
-
-  // fs.readFile("./data/posts.json", "utf-8", (error, existingData) => {
-  //   if (error) {
-  //     res.status(500).json({ error: error.constructor.name, message: error.message })
-  //     return
-  //   }
-
-  //   const posts = JSON.parse(existingData)
-  //   posts.push(post)
-
-  //   post.date = utils.getDateStringDayMonthYearFormat()
-  //   post.id = `${Math.random().toString().slice(2)}-${Date.now()}`
-
-  //   const postsJson = JSON.stringify(posts)
-
-  //   fs.writeFile("./data/posts.json", postsJson, error => {
-  //     if (error) {
-  //       res.status(500).json({ error: error.constructor.name, message: error.message })
-  //       return
-  //     }
-  //     res.status(201).send(post)
-  //   })
-  // })
 
   const username = req.headers.authorization.slice(6) // cabezera para la autenticacion del usuario
   const { title, image, description, } = req.body
 
 
   try {
+
     logic.createPost(username, title, image, description, (error) => {
 
       if (error) {
@@ -220,6 +127,7 @@ api.post("/posts", jsonBodyParser, (req, res) => {
 
       res.status(201).send()
     })
+
 
   } catch (error) {
 
@@ -254,4 +162,6 @@ api.delete("/posts/:postId", (req, res) => {
   }
 })
 
-api.listen(8080, () => console.log('listening on port http://localhost:8080/app2/register'))
+
+
+api.listen(8080, () => console.log('listening on port http://localhost:8080/app/login'))
