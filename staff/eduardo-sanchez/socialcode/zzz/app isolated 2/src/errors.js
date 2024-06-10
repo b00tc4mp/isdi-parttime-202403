2e4e249cf24860cac08a40cc8655c543870bfb02
Eduardolans <eduard.criado@gmail.com>
2024-06-10 16:05:54 +0200

    extract atomic, molecular and container core components #136

---
+++ staff/eduardo-sanchez/socialcode/zzz/app isolated 2/src/errors.js
@@ -0,0 +1,41 @@
+class ContentError extends Error {
+    constructor(message) {
+        super(message)
+
+        //this.name = ContentError.name
+        this.name = this.constructor.name
+    }
+}
+
+class MatchError extends Error {
+    constructor(message) {
+        super(message)
+
+        this.name = this.constructor.name
+    }
+}
+
+class DuplicityError extends Error {
+    constructor(message) {
+        super(message)
+
+        this.name = this.constructor.name
+    }
+}
+
+class SystemError extends Error {
+    constructor(message) {
+        super(message)
+
+        this.name = this.constructor.name
+    }
+}
+
+const errors = {
+    ContentError,
+    MatchError,
+    DuplicityError,
+    SystemError
+}
+
+export default errors
