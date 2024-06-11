f6a149ae9e2f02e24a372a13a41be34e52b0b0b2
Eduardolans <eduard.criado@gmail.com>
2024-06-11 19:23:45 +0200

    add heading & image core components; update title & view compos; add user name retrieval & rendering in home; add post list compo in home with posts retrieval & rendering #136

--- staff/eduardo-sanchez/socialcode/app/src/views/Home.jsx
+++ staff/eduardo-sanchez/socialcode/app/src/views/Home.jsx
@@ -1,13 +1,19 @@
-// import Title from '../components/core/Title'
+import { useState, useEffect } from 'react'
+
 import View from '../components/library/View'
 
 import Header from './components/Header'
+import PostList from './components/PostList'
 
 import Button from '../components/core/Button'
+import Heading from '../components/core/Heading'
+
 import logic from '../logic'
 
 function Home({ onUserLoggedOut }) {
     console.log('Home -> render')
+
+    const [name, setName] = useState('')
 
     const handleLogout = () => {
         logic.logoutUser()
@@ -15,13 +21,39 @@ function Home({ onUserLoggedOut }) {
         onUserLoggedOut()
     }
 
+    useEffect(() => {
+        console.log('Home -> useEffect')
+        // setTimeout(() => {
+        try {
+            logic.getUserName((error, name) => {
+                if (error) {
+                    console.error(error)
+
+                    alert(error.message)
+
+                    return
+                }
+
+                console.log('Home -> setName')
+
+                setName(name)
+            })
+        } catch (error) {
+            console.error(error)
+
+            alert(error.message)
+        }
+        // }, 10000)
+    }, [])
+
     return <View>
         <Header>
+            <Heading level="3">{name}</Heading>
             <Button onClick={handleLogout}>Logout</Button>
         </Header>
 
         <View tag="main">
-
+            <PostList />
         </View>
 
         <footer></footer>
