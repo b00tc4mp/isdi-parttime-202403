0c9a08d6e244493794efadd6935a7d88d0bd6a33
Jose A. Canto <jose.a.c.lopez@gmail.com>
2024-06-14 20:44:47 +0200

    add component confirm delete with functionality, and css styles #131

--- staff/jose-canto/socialcode/app3_react_vite/src/components/views/components/Post.jsx
+++ staff/jose-canto/socialcode/app3_react_vite/src/components/views/components/Post.jsx
@@ -1,29 +1,40 @@
+import { useState } from "react"
+
 import Image from "../../core/Image"
 import Heading from "../../core/Heading"
 import Button from "../../core/Button"
 import Text from "../../core//Text"
 import Time from "../../views/components/Time"
+import ConfirmDelete from "./ConfirmDelete"
 
 import logic from "../../../logic"
 
 function Post({ post, onPostDeleted }) {
 	console.log("Post --> render")
-	const handleDeletePost = () => {
-		if (confirm("Delete this post?")) {
-			try {
-				logic.deletePost(post.id, (error) => {
-					if (error) {
-						console.error(error)
-						return
-					}
 
-					onPostDeleted()
-				})
-			} catch (error) {
-				console.error(error)
-				alert(error.message)
-			}
+	const [showConfirmDelete, setShowConfirmDelete] = useState(false)
+
+	const handleDeletePost = () => setShowConfirmDelete(true)
+
+	const confirmDeletePost = () => {
+		try {
+			logic.deletePost(post.id, (error) => {
+				if (error) {
+					console.error(error)
+					return
+				}
+
+				onPostDeleted()
+				setShowConfirmDelete(false)
+			})
+		} catch (error) {
+			console.error(error)
+			alert(error.message)
 		}
+	}
+
+	const cancelDeletePost = () => {
+		setShowConfirmDelete(false)
 	}
 
 	return (
@@ -39,6 +50,9 @@ function Post({ post, onPostDeleted }) {
 
 				<div className="DivImage">
 					<Image className="Image" src={post.image}></Image>
+					{showConfirmDelete && (
+						<ConfirmDelete post={post} onConfirmDeletePost={confirmDeletePost} onCancelDeletePost={cancelDeletePost} />
+					)}
 				</div>
 
 				<Time>{post.date}</Time>
