501d0df0ed229c42a1b76798f4b2022aa94e1ae0
Eduardolans <eduard.criado@gmail.com>
2024-06-18 16:24:11 +0200

    componetize create post form, and others; add more features to view compo; add feedback panel to form with feedback #136

---
+++ staff/eduardo-sanchez/socialcode/zzz/app isolated 11/src/components/library/View.jsx
@@ -0,0 +1,20 @@
+import './View.css'
+
+function View({ tag: Tag = 'div', className, children, direction = 'column', align = 'center' }) {
+
+    return <Tag className={`View ${className ? className : ''} ${direction === 'column' ? 'View--column' : 'View--row'} ${align === 'center' ? 'View--center' : ''}`}>{children}</Tag>
+}
+
+export default View
+
+{/*
+    
+import './View.css'
+
+function View({ tag: Tag = 'div', className, children, direction = 'column', align = 'center' }) {
+    return <Tag className={`View ${className ? className : ''} ${direction === 'column' ? 'View column' : 'View row'} ${align === 'center' ? 'View center' : ''}`}>{children}</Tag>
+}
+
+export default View
+
+*/}
