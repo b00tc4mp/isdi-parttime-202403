1971f6c83c85d81a62f29ba33fc6aec939c58b08
Manuel Barzi <manuelbarzi@gmail.com>
2024-06-19 21:46:23 +0200

    split api logic into files; add com package with errors and validations; apply com in app and api; #119

---
+++ staff/manuel-barzi/socialcode/zzz/api.12/logic/createPost.js
@@ -0,0 +1,55 @@
+import data from '../data/index.js'
+import { ContentError, MatchError } from '../errors.js'
+
+const USERNAME_REGEX = /^[\w-]+$/
+
+const createPost = (username, title, image, description, callback) => {
+    if (!USERNAME_REGEX.test(username))
+        throw new ContentError('username is not valid')
+
+    if (typeof title !== 'string' || !title.length || title.length > 50)
+        throw new ContentError('title is not valid')
+
+    if (typeof image !== 'string' || !image.startsWith('http'))
+        throw new ContentError('image is not valid')
+
+    if (typeof description !== 'string' || !description.length || description.length > 200)
+        throw new ContentError('description is not valid')
+
+    if (typeof callback !== 'function')
+        throw new TypeError('callback is not a function')
+
+    data.findUser(user => user.username === username, (error, user) => {
+        if (error) {
+            callback(error)
+
+            return
+        }
+
+        if (!user) {
+            callback(new MatchError('user not found'))
+
+            return
+        }
+
+        const post = {
+            author: username,
+            title,
+            image,
+            description,
+            date: new Date().toISOString()
+        }
+
+        data.insertPost(post, error => {
+            if (error) {
+                callback(error)
+
+                return
+            }
+
+            callback(null)
+        })
+    })
+}
+
+export default createPost
