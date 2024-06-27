2612e04bda255933251d754457ddea606c64579c
Jose A. Canto <jose.a.c.lopez@gmail.com>
2024-06-27 12:47:09 +0200

    ejemplo cors implement socialcode app #131

--- staff/jose-canto/socialcode/api/cors.js
+++ staff/jose-canto/socialcode/api/cors.js
@@ -1,4 +1,4 @@
-
+// ejemplo de cors sin usar dependencias externas
 export default api.use("*", (req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*")
   res.setHeader("Access-Control-Allow-Methods", "*")
