50c764311cdca3fe90d4e14ea905f95b2a8ccb8e
Eduardolans <eduard.criado@gmail.com>
2024-07-12 19:10:43 +0200

    apply handle-error-response in all route handlers & separate handle error routes in api; #136

---
+++ staff/eduardo-sanchez/socialcode/zzz/10-07/api 10-07/logic/toggleLikePost.test.js
@@ -0,0 +1,18 @@
+import 'dotenv/config'
+import mongoose from 'mongoose'
+
+import toggleLikePost from './toggleLikePost.js'
+
+const { MONGODB_URL } = process.env
+
+mongoose.connect(MONGODB_URL)
+    .then(() => {
+        try {
+            toggleLikePost('668bf6db2c12b9a873e4cd2f', '668bf3479e69ccb19311ac94')
+                .then(() => console.log('user toggled like'))
+                .catch(error => console.error(error))
+        } catch (error) {
+            console.error(error)
+        }
+    })
+    .catch(error => console.error(error))
