7fe9e333ca9134bb2052e0c1f7617e91c3626dd0
Manuel Barzi <manuelbarzi@gmail.com>
2024-07-15 21:42:17 +0200

    split api server handlers; add error handler; add test for authenticate user with mocha and chai, #119

---
+++ staff/manuel-barzi/socialcode/zzz/api.26/logic/getUserName.test.js
@@ -0,0 +1,18 @@
+import 'dotenv/config'
+import mongoose from 'mongoose'
+
+import getUserName from './getUserName.js'
+
+const { MONGODB_URL } = process.env
+
+mongoose.connect(MONGODB_URL)
+    .then(() => {
+        try {
+            getUserName('66859a59d54c3bf8f5e205da', '66859f3b6ef36441e7efcd0d')
+                .then(name => console.log('user name retrieved', name))
+                .catch(error => console.error(error))
+        } catch (error) {
+            console.error(error)
+        }
+    })
+    .catch(error => console.error(error))
