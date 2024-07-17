745bd5c516268a0cb09ff7283839bee2454f1b92
Eduardolans <eduard.criado@gmail.com>
2024-07-17 18:48:47 +0200

    update implementation of authenticate user spec & add happy path test case in create post spec; #136

---
+++ staff/eduardo-sanchez/socialcode/zzz/15-07/api 15-07/logic/getUserName.test.js
@@ -0,0 +1,18 @@
+import 'dotenv/config'
+import mongoose from 'mongoose'
+
+import getUserName from './getUserName.js'
+
+const { MONGODB_URL } = process.env
+
+mongoose.connect(MONGODB_URL)
+    .then(() => {
+        try {
+            getUserName('668bf6db2c12b9a873e4cd2f', '668bf0d070e99e711ec35ee0')
+                .then(name => console.log('user name retrieved', name))
+                .catch(error => console.error(error))
+        } catch (error) {
+            console.error(error)
+        }
+    })
+    .catch(error => console.error(error))
