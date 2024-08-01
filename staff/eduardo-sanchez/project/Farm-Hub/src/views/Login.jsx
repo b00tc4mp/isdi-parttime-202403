bb3b90b2a9321fe415570bfbb10b97205b3269b1
Eduardolans <eduard.criado@gmail.com>
2024-08-01 21:38:56 +0200

    add state for the username in the Login form; #165

--- staff/eduardo-sanchez/project/Farm-Hub/src/views/Login.jsx
+++ staff/eduardo-sanchez/project/Farm-Hub/src/views/Login.jsx
@@ -1,22 +1,34 @@
+import { useState } from "react"
+
 export const Login = () => {
     const onLoginClick = ({ username, password }) => {
         console.log("Welcome ", `${username} with password ${password}`)
     }
 
-    const handleUsername = (event) => {
-        console.log(`Username: -> ${event.target.value}`)
-        event.preventDefault()
-    }
 
     const handlePassword = (event) => {
         console.log(`Password: -> ${event.target.value}`)
         event.preventDefault()
     }
 
+
+    const [userName, setUsername] = useState('')
+
     return <>
-        <input placeholder="username" onChange={handleUsername} id="username"></input>
-        <input placeholder="password" onChange={handlePassword} ></input>
-        <button onClick={() => onLoginClick({ username: '', password: '' })}>Login</button>
+
+        <input
+            placeholder="username"
+            id="username"
+            onChange={(newText) => {
+                setUsername(newText.target.value)
+            }}
+        ></input>
+        <input
+            placeholder="password"
+            onChange={handlePassword}
+        ></input>
+
+        <button onClick={() => onLoginClick({ username: userName, password: '' })}>Login</button>
     </>
 }
 
