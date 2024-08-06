13291ef2a9e73076486ee144397dcf67d6a529a1
Jorge Hernández Moreno <magicjorge@gmail.com>
2024-08-06 18:32:01 +0200

    Remove ignored files

--- staff/jorge-hernandez/project/ShowConnect/app/src/components/ArtistProfile.jsx
+++ staff/jorge-hernandez/project/ShowConnect/app/src/components/ArtistProfile.jsx
@@ -14,7 +14,7 @@ function ArtistProfile({ artist, onClose }) {
   }
 
   return (
-    <div className='fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50'>
+    <div className='fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-60'>
       <div className='bg-gray-800 text-white rounded-lg p-6 w-full max-w-4xl relative'>
         <button className='absolute top-4 right-4 text-2xl' onClick={onClose}>
           &times;
