7a1942c7fdd94249362ed90b90124e4c7a81dc55
Sor91 <soraya.sv91@gmail.com>
2024-08-15 18:21:26 +0100

    update logic test for getMyInProgressTasks and getMyFinishedTasks in api  #176

--- staff/soraya-suarez/project/api/logic/getMyFinishedTasks.test.js
+++ staff/soraya-suarez/project/api/logic/getMyFinishedTasks.test.js
@@ -1,14 +1,14 @@
 import 'dotenv/config'
 import mongoose from 'mongoose'
 
-import getMyInProgressTasks from './getMyInProgressTasks.js'
+import getMyFinishedTasks from './getMyFinishedTasks.js'
 
 const { MONGODB_URL } = process.env
 
 mongoose.connect(MONGODB_URL)
     .then(() => {
         try {
-            getMyInProgressTasks('66b4c032d54497eae7195f22', (error, posts) => {
+            getMyFinishedTasks('66b4c032d54497eae7195f22', (error, posts) => {
                 if (error) {
                     console.error(error)
 
