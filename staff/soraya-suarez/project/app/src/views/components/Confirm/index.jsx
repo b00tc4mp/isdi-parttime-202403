f1d4da54b3781dfec5e9e603c0b7e03569d8b973
Sor91 <soraya.sv91@gmail.com>
2024-08-21 16:13:46 +0100

    upgrade design for confirm component  #176

--- staff/soraya-suarez/project/app/src/views/components/Confirm/index.jsx
+++ staff/soraya-suarez/project/app/src/views/components/Confirm/index.jsx
@@ -1,9 +1,8 @@
 import './index.css'
 
-export default ({ message, onAccept, onCancel }) => <div className="Confirm">
-    <div className="ConfirmBox">
+export default ({ message, onAccept, onCancel }) => 
+    <div className="container-center min-h-screen">
         <p>{message}</p>
         <button className="Button" onClick={onCancel}>Cancel</button>
         <button className="Button" onClick={onAccept}>Confirm</button>
     </div>
-</div>
