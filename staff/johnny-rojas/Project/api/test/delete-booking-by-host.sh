7f247bdbebc55b30b76958f0eb40bdc1182363d9
Johnny Rojas Cifra <157323400+Johnnyrc26@users.noreply.github.com>
2024-08-28 14:35:44 +0200

    Update delete-booking-by-host.sh test script #172

--- staff/johnny-rojas/Project/api/test/delete-booking-by-host.sh
+++ staff/johnny-rojas/Project/api/test/delete-booking-by-host.sh
@@ -1,4 +1,4 @@
-curl -X DELETE http://localhost:8080/rooms/66cdc7df37c86049d90becbb/manage \
+curl -X DELETE http://localhost:8080/rooms/66cdcacd37c86049d90bece1/manage/booking \
 -H "Content-Type: application/json" \
--H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NmNkYzczMTM3Yzg2MDQ5ZDkwYmVjYjUiLCJyb2xlIjoiaG9zdCIsImlhdCI6MTcyNDc4MjcxMCwiZXhwIjoxNzI1MTI4MzEwfQ.pO9-hrd_9xGl24LCqThNv1bpmYjZKW-c9ijcszuqpRE" \
--d '{"userId": "66cdc73137c86049d90becb5", "roomId": "66cdc7df37c86049d90becbb", "bookingId": "66cf0024cfa544493d959d7a"}' -v
+-H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NmNkYzg4ODM3Yzg2MDQ5ZDkwYmVjYzAiLCJyb2xlIjoiZ3Vlc3QiLCJpYXQiOjE3MjQ3NjIyNjIsImV4cCI6MTcyNTEwNzg2Mn0.Eeg4m7bGg0rhT3h61UDhaKMBNjjmQ1kvvSzch6zCK_8" \
+-d '{"userId": "66cdc88837c86049d90becc0", "roomId": "66cdcacd37c86049d90bece1", "bookingId": "66cf170581f09f39cb445acb"}' -v
