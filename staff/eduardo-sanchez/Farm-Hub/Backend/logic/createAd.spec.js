ed675aa332fb6fdef9130b4f95ba71925c6fc9b2
Eduardolans <eduard.criado@gmail.com>
2024-09-10 20:13:21 +0200

    add spec test for createAdComment; update spec test with Objectid in createAd; #165

--- staff/eduardo-sanchez/Farm-Hub/Backend/logic/createAd.spec.js
+++ staff/eduardo-sanchez/Farm-Hub/Backend/logic/createAd.spec.js
@@ -9,6 +9,8 @@ import { NotFoundError, ContentError } from 'com/errors.js'
 
 const { MONGODB_URL_TEST } = process.env
 
+const { ObjectId } = Types
+
 describe('createAd', () => {
     before(() => mongoose.connect(MONGODB_URL_TEST).then(() => Promise.all([User.deleteMany(), Ad.deleteMany()])))
 
@@ -26,7 +28,8 @@ describe('createAd', () => {
                         expect(ad.title).to.equal('Limones')
                         expect(ad.description).to.equal('Luneros')
                         expect(ad.price).to.equal('8.5 €/Kg')
-
+                        expect(ad.date).to.be.a('date')
+                        expect(ad.adcomments).to.be.an('array') //.that.is.empty 
                     })
             )
     )
@@ -34,7 +37,16 @@ describe('createAd', () => {
     it('fails on non-existing user', () => {
         let errorThrown
 
-        return createAd('123456789012345678901234', 'Ad Title', 'Ad Description', '8.5 €/Kg')
+        // return bcrypt.hash("123123123", 8)
+        //     .then(hash => User.create({ name: "Amigo", surname: "Mio", email: "amigo@mio.com", username: "amigomio", password: hash }))
+        //     .then(() => createAd(new ObjectId().toString(), 'Test', 'Funcionando', '8.5 €/Kg', []))
+        //     .catch(error => errorThrown = error)
+        //     .finally(() => {
+        //         expect(errorThrown).to.be.an.instanceOf(NotFoundError)
+        //         expect(errorThrown.message).to.equal("user not found")
+        //     })
+
+        return createAd(new ObjectId().toString(), 'Ad Title', 'Ad Description', '8.5 €/Kg')
             .catch(error => errorThrown = error)
             .finally(() => {
                 expect(errorThrown).to.be.instanceOf(NotFoundError)
@@ -55,52 +67,64 @@ describe('createAd', () => {
         }
     })
 
+    // it('fails on invalid userId', () => {
+    //     expect(() => createAd('invalid-id', 'Ad Title', 'Ad Description', '8.5 €/Kg'))
+    //         .to.throw(ContentError, 'userId is not valid')
+    // })
+
+
     it('fails on invalid title', () => {
         let errorThrown
 
-        return User.create({ name: 'John', surname: 'Doe', email: 'john@doe.com', username: 'johndoe', password: '12345678' })
-            .then(user => {
-                try {
-                    createAd(user._id.toString(), '', 'Ad Description', '8.5 €/Kg')
-                } catch (error) {
-                    errorThrown = error
-                } finally {
-                    expect(errorThrown).to.be.instanceOf(ContentError)
-                    expect(errorThrown.message).to.equal('title is not valid')
-                }
-            })
+
+        try {
+            createAd(new ObjectId().toString(), '', 'Ad Description', '8.5 €/Kg')
+        } catch (error) {
+            errorThrown = error
+        } finally {
+            expect(errorThrown).to.be.instanceOf(ContentError)
+            expect(errorThrown.message).to.equal('title is not valid')
+        }
     })
+
+    // it('fails on invalid title', () => {
+    //     return createAd(new ObjectId().toString(), '', 'Ad Description', '8.5 €/Kg')
+    //         .catch(error => {
+    //             expect(error).to.be.instanceOf(ContentError)
+    //             expect(error.message).to.equal('title is not valid')
+    //         })
+    // })
+
 
     it('fails on invalid description', () => {
         let errorThrown
 
-        return User.create({ name: 'John', surname: 'Doe', email: 'john@doe.com', username: 'johndoe', password: '12345678' })
-            .then(user => {
-                try {
-                    createAd(user._id.toString(), 'Ad Title', '', '8.5 €/Kg')
-                } catch (error) {
-                    errorThrown = error
-                } finally {
-                    expect(errorThrown).to.be.instanceOf(ContentError)
-                    expect(errorThrown.message).to.equal('description is not valid')
-                }
-            })
+        // return User.create({ name: 'John', surname: 'Doe', email: 'john@doe.com', username: 'johndoe', password: '12345678' })
+        // .then(user => {
+        try {
+            createAd(new ObjectId().toString(), 'Ad Title', '', '8.5 €/Kg')
+        } catch (error) {
+            errorThrown = error
+        } finally {
+            expect(errorThrown).to.be.instanceOf(ContentError)
+            expect(errorThrown.message).to.equal('description is not valid')
+        }
     })
 
     it('fails on invalid price', () => {
         let errorThrown
 
-        return User.create({ name: 'John', surname: 'Doe', email: 'john@doe.com', username: 'johndoe', password: '12345678' })
-            .then(user => {
-                try {
-                    createAd(user._id.toString(), 'Ad Title', 'Ad Description', '8.5')
-                } catch (error) {
-                    errorThrown = error
-                } finally {
-                    expect(errorThrown).to.be.instanceOf(ContentError)
-                    expect(errorThrown.message).to.equal('price is not valid. It must be in the format "number €/Kg", e.g., "3.20 €/Kg".');
-                }
-            });
+        // return User.create({ name: 'John', surname: 'Doe', email: 'john@doe.com', username: 'johndoe', password: '12345678' })
+        //     .then(user => {
+        try {
+            createAd(new ObjectId().toString(), 'Ad Title', 'Ad Description', '8.5')
+        } catch (error) {
+            errorThrown = error
+        } finally {
+            expect(errorThrown).to.be.instanceOf(ContentError)
+            expect(errorThrown.message).to.equal('price is not valid. It must be in the format "number €/Kg", e.g., "3.20 €/Kg".');
+        }
+        // });
     })
 
     after(() => Promise.all([User.deleteMany(), Ad.deleteMany()]).then(() => mongoose.disconnect()))
---
+++ staff/eduardo-sanchez/Farm-Hub/Backend/logic/createAdComment.spec.js
@@ -0,0 +1,176 @@
+import 'dotenv/config'
+import mongoose, { Types } from 'mongoose'
+import bcrypt from 'bcryptjs'
+import { expect } from 'chai'
+import { Ad, User } from '../data/index.js'
+
+import createAdComment from './createAdComment.js'
+import { NotFoundError, ContentError } from 'com/errors.js'
+
+const { MONGODB_URL_TEST } = process.env
+const { ObjectId } = Types
+
+
+describe('createAdComment', () => {
+    before(() => mongoose.connect(MONGODB_URL_TEST).then(() => Promise.all([User.deleteMany(), Ad.deleteMany()])))
+
+
+    beforeEach(() => Promise.all([User.deleteMany(), Ad.deleteMany()]))
+
+    it('succeeds on adding a comment to an existing ad by an existing user', () =>
+        bcrypt.hash('123123123', 8)
+            .then(hash => User.create({ name: 'Li', surname: 'Nux', email: 'li@nux.com', username: 'linux', password: hash }))
+            .then(user =>
+                Ad.create({ author: user._id.toString(), title: 'Limones', description: 'Luneros', price: '8.5 €/Kg', date: new Date(), adcomments: [] })
+                    .then(ad =>
+                        createAdComment(user.id, ad.id, 'Nice Ad!')
+                            .then(() => Ad.findById(ad.id))
+                            .then(ad => {
+
+                                expect(ad.adcomments).to.have.lengthOf(1)
+                                expect(ad.adcomments[0].author.toString()).to.equal(user.id.toString())
+                                expect(ad.adcomments[0].comment).to.equal('Nice Ad!')
+                                expect(ad.adcomments[0].date).to.be.a('date')
+                            })
+                    )
+            )
+    )
+
+    it('fails on non-existing user', () => {
+        let errorThrown
+
+        return Ad.create({ author: new ObjectId(), title: 'Limones', description: 'Luneros', price: '8.5 €/Kg', date: new Date(), adcomments: [] })
+            .then(ad =>
+                createAdComment(new ObjectId().toString(), ad.id, 'Nice Ad!')
+                    .catch(error => errorThrown = error)
+                    .finally(() => {
+                        expect(errorThrown).to.be.instanceOf(NotFoundError)
+                        expect(errorThrown.message).to.equal('user not found')
+                    })
+            )
+    })
+
+    it('fails on non-existing ad', () => {
+        let errorThrown
+
+        return bcrypt.hash('123123123', 8)
+            .then(hash => User.create({ name: 'Li', surname: 'Nux', email: 'li@nux.com', username: 'linux', password: hash }))
+            .then(user =>
+                createAdComment(user.id, new ObjectId().toString(), 'Nice Ad!')
+                    .catch(error => errorThrown = error)
+                    .finally(() => {
+                        expect(errorThrown).to.be.instanceOf(NotFoundError)
+                        expect(errorThrown.message).to.equal('ad not found')
+                    })
+            )
+    })
+
+    it('fails on invalid userId', () => {
+        let errorThrown
+
+        try {
+            createAdComment('invalid-id', new ObjectId().toString(), 'Nice Ad!')
+        } catch (error) {
+            errorThrown = error
+        } finally {
+            expect(errorThrown).to.be.instanceOf(ContentError)
+            expect(errorThrown.message).to.equal('userId is not valid')
+        }
+    })
+
+    it('fails on invalid adId', () => {
+        let errorThrown
+
+        try {
+            createAdComment(new ObjectId().toString(), 'invalid-id', 'Nice Ad!')
+        } catch (error) {
+            errorThrown = error
+        } finally {
+            expect(errorThrown).to.be.instanceOf(ContentError)
+            expect(errorThrown.message).to.equal('adId is not valid')
+        }
+    })
+
+    it('fails on invalid comment text', () => {
+        let errorThrown
+
+        try {
+            createAdComment(new ObjectId().toString(), new ObjectId().toString(), '')
+        } catch (error) {
+            errorThrown = error
+        } finally {
+            expect(errorThrown).to.be.instanceOf(ContentError)
+            expect(errorThrown.message).to.equal('comment is not valid')
+        }
+
+    })
+    after(() => Ad.deleteMany().then(() => User.deleteMany()).then(() => mongoose.disconnect()))
+
+})
+
+
+
+// it('succeeds on creating an ad comment from an existing user', () => {
+//     return bcrypt.hash('123123123', 8)
+//         .then(hash => User.create({ name: 'Li', surname: 'Nux', email: 'li@nux.com', username: 'linux', password: hash }))
+//         .then(user =>
+//             createAdComments(user.id, new ObjectId().toString(), 'This is a comment')
+//                 .then(() => Ad.findOne())
+//                 .then(ad => {
+//                     expect(ad.adcomments).to.be.an('array').that.is.not.empty
+//                     expect(ad.adcomments[0].author.toString()).to.equal(user.id.toString())
+//                     expect(ad.adcomments[0].comment).to.equal('This is a comment')
+
+//                 })
+//         )
+// })
+
+// it('fails on non-existing user', () => {
+//     let errorThrown
+
+//     return createAdComment(new ObjectId().toString(), new ObjectId().toString(), 'Nice Ad!')
+//         .catch(error => errorThrown = error)
+
+//         .finally(() => {
+//             expect(errorThrown).to.be.instanceOf(NotFoundError)
+//             expect(errorThrown.message).to.equal('user not found')
+//         })
+// })
+
+// it('fails on invalid adId', () => {
+//     let errorThrown
+
+//     return bcrypt.hash('123123123', 8)
+//         .then(hash => User.create({ name: 'Li', surname: 'Nux', email: 'li@nux.com', username: 'linux', password: hash }))
+//         .then(user => {
+//             try {
+//                 createAdComment(user.id, 'invalid-id', 'Nice Ad!')
+//             } catch (error) {
+//                 errorThrown = error
+//             } finally {
+//                 expect(errorThrown).to.be.instanceOf(ContentError)
+//                 expect(errorThrown.message).to.equal('adId is not valid')
+//             }
+//         })
+// })
+
+
+// it('fails on invalid comment text', () => {
+//     let errorThrown
+
+//     return bcrypt.hash('123123123', 8)
+//         .then(hash => User.create({ name: 'Li', surname: 'Nux', email: 'li@nux.com', username: 'linux', password: hash }))
+//         .then(user =>
+//             Ad.create({ author: user._id.toString(), title: 'Limones', description: 'Luneros', price: '8.5 €/Kg', date: new Date(), adcomments: [] })
+//                 .then(ad => {
+//                     try {
+//                         createAdComment(user.id, ad.id, '')
+//                     } catch (error) {
+//                         errorThrown = error
+//                     } finally {
+//                         expect(errorThrown).to.be.instanceOf(ContentError)
+//                         expect(errorThrown.message).to.equal('comment is not valid')
+//                     }
+//                 })
+//         )
+// })
