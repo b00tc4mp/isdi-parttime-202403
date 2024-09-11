79f88bb25fce68c8c08f0251595d507f94561266
Eduardolans <eduard.criado@gmail.com>
2024-09-11 13:51:49 +0200

    add spec test for deleteAdComment logic; update styles to show text below search box when there are no ads; #165

--- staff/eduardo-sanchez/Farm-Hub/Backend/logic/deleteAd.spec.js
+++ staff/eduardo-sanchez/Farm-Hub/Backend/logic/deleteAd.spec.js
@@ -1,133 +1,163 @@
-import 'dotenv/config'
-import mongoose, { Types } from 'mongoose'
-import bcrypt from 'bcryptjs'
-import { expect } from 'chai'
-import { Ad, User } from '../data/index.js'
+import "dotenv/config";
+import mongoose, { Types } from "mongoose";
+import bcrypt from "bcryptjs";
+import { expect } from "chai";
+import { Ad, User } from "../data/index.js";
 
-import deleteAd from './deleteAd.js'
-import { NotFoundError, ContentError, MatchError } from 'com/errors.js'
+import deleteAd from "./deleteAd.js";
+import { NotFoundError, ContentError, MatchError } from "com/errors.js";
 
-const { MONGODB_URL_TEST } = process.env
-const { ObjectId } = Types
+const { MONGODB_URL_TEST } = process.env;
+const { ObjectId } = Types;
 
+describe("deleteAd", () => {
+  before(() =>
+    mongoose
+      .connect(MONGODB_URL_TEST)
+      .then(() => Promise.all([User.deleteMany(), Ad.deleteMany()]))
+  );
 
-describe('deleteAd', () => {
-    before(() => mongoose.connect(MONGODB_URL_TEST).then(() => Promise.all([User.deleteMany(), Ad.deleteMany()])))  // before all tests
+  beforeEach(() => Promise.all([User.deleteMany(), Ad.deleteMany()]));
 
-    beforeEach(() => Promise.all([User.deleteMany(), Ad.deleteMany()]))
-
-
-    it('succeeds on deleting an ad by its author', () =>
-        bcrypt.hash('123123123', 8)
-            .then(hash => User.create({ name: 'Li', surname: 'Nux', email: 'li@nux.com', username: 'linux', password: hash }))
-            .then(user =>
-                Ad.create({ author: user._id.toString(), title: 'Limones', description: 'Luneros', price: '8.5 €/Kg', date: new Date(), adcomments: [] })
-                    .then(ad =>
-                        deleteAd(user.id, ad.id)
-                            .then(() => Ad.findById(ad.id))
-                            .then(deletedAd => {
-                                expect(deletedAd).to.be.null
-                            })
-                    )
-            )
-    )
-
-
-    it('fails on non-existing user', () => {
-        let errorThrown
-
-        return Ad.create({ author: new ObjectId(), title: 'Limones', description: 'Luneros', price: '8.5 €/Kg', date: new Date() })
-            .then(ad =>
-                deleteAd(new ObjectId().toString(), ad.id)
-                    .catch(error => errorThrown = error)
-                    .finally(() => {
-                        expect(errorThrown).to.be.instanceOf(NotFoundError)
-                        expect(errorThrown.message).to.equal('user not found')
-                    })
-            )
-    })
-
-
-    it('fails on non-existing ad', () => {
-        let errorThrown
-
-        return bcrypt.hash('123123123', 8)
-            .then(hash => User.create({ name: 'Li', surname: 'Nux', email: 'li@nux.com', username: 'linux', password: hash }))
-            .then(user =>
-                deleteAd(user.id, new ObjectId().toString())
-                    .catch(error => errorThrown = error)
-                    .finally(() => {
-                        expect(errorThrown).to.be.instanceOf(NotFoundError)
-                        expect(errorThrown.message).to.equal('ad not found')
-                    })
-            )
-    })
-
-
-    it('fails on invalid userId', () => {
-        let errorThrown
-
-        try {
-            deleteAd('invalid-id', new ObjectId().toString())
-        } catch (error) {
-            errorThrown = error
-        } finally {
-            expect(errorThrown).to.be.instanceOf(ContentError)
-            expect(errorThrown.message).to.equal('userId is not valid')
-        }
-    })
-
-
-    it('fails on invalid adId', () => {
-        let errorThrown
-
-        try {
-            deleteAd(new ObjectId().toString(), 'invalid-id')
-        } catch (error) {
-            errorThrown = error
-        } finally {
-            expect(errorThrown).to.be.instanceOf(ContentError)
-            expect(errorThrown.message).to.equal('adId is not valid')
-        }
-    })
-
-    it('fails on ad author does not match user wanting to delete ad', () => {
-        let errorThrown
-
-        return bcrypt.hash('123123123', 8)
-            .then(hash => {
-                const user = new User({
-                    name: 'Li',
-                    surname: 'Nux',
-                    email: 'li@nux.com',
-                    username: 'linux',
-                    password: hash
-                })
-                const ad = new Ad({
-                    author: new ObjectId().toString(),
-                    title: 'Limones',
-                    description: 'Luneros',
-                    price: '10 €/Kg',
-                    date: new Date(),
-                    adcomments: []
-                })
-                return Promise.all([user.save(), ad.save()])
-                    .then(([savedUser, savedAd]) => {
-                        return deleteAd(savedUser.id.toString(), savedAd.id.toString())
-                    })
-                    .catch(error => errorThrown = error)
+  it("succeeds on deleting an ad by its author", () =>
+    bcrypt
+      .hash("123123123", 8)
+      .then((hash) =>
+        User.create({
+          name: "Li",
+          surname: "Nux",
+          email: "li@nux.com",
+          username: "linux",
+          password: hash,
+        })
+      )
+      .then((user) =>
+        Ad.create({
+          author: user._id.toString(),
+          title: "Limones",
+          description: "Luneros",
+          price: "8.5 €/Kg",
+          date: new Date(),
+          adcomments: [],
+        }).then((ad) =>
+          deleteAd(user.id, ad.id)
+            .then(() => Ad.findById(ad.id))
+            .then((deletedAd) => {
+              expect(deletedAd).to.be.null;
             })
-            .finally(() => {
-                expect(errorThrown).to.be.an.instanceOf(MatchError)
-                expect(errorThrown.message).to.equal('ad author does not match user & cannot be deleted')
-            })
-    })
+        )
+      ));
 
-    after(() => Ad.deleteMany().then(() => User.deleteMany()).then(() => mongoose.disconnect()))
+  it("fails on non-existing user", () => {
+    let errorThrown;
 
-})
+    return Ad.create({
+      author: new ObjectId(),
+      title: "Limones",
+      description: "Luneros",
+      price: "8.5 €/Kg",
+      date: new Date(),
+    }).then((ad) =>
+      deleteAd(new ObjectId().toString(), ad.id)
+        .catch((error) => (errorThrown = error))
+        .finally(() => {
+          expect(errorThrown).to.be.instanceOf(NotFoundError);
+          expect(errorThrown.message).to.equal("user not found");
+        })
+    );
+  });
 
+  it("fails on non-existing ad", () => {
+    let errorThrown;
 
+    return bcrypt
+      .hash("123123123", 8)
+      .then((hash) =>
+        User.create({
+          name: "Li",
+          surname: "Nux",
+          email: "li@nux.com",
+          username: "linux",
+          password: hash,
+        })
+      )
+      .then((user) =>
+        deleteAd(user.id, new ObjectId().toString())
+          .catch((error) => (errorThrown = error))
+          .finally(() => {
+            expect(errorThrown).to.be.instanceOf(NotFoundError);
+            expect(errorThrown.message).to.equal("ad not found");
+          })
+      );
+  });
+
+  it("fails on invalid userId", () => {
+    let errorThrown;
+
+    try {
+      deleteAd("invalid-id", new ObjectId().toString());
+    } catch (error) {
+      errorThrown = error;
+    } finally {
+      expect(errorThrown).to.be.instanceOf(ContentError);
+      expect(errorThrown.message).to.equal("userId is not valid");
+    }
+  });
+
+  it("fails on invalid adId", () => {
+    let errorThrown;
+
+    try {
+      deleteAd(new ObjectId().toString(), "invalid-id");
+    } catch (error) {
+      errorThrown = error;
+    } finally {
+      expect(errorThrown).to.be.instanceOf(ContentError);
+      expect(errorThrown.message).to.equal("adId is not valid");
+    }
+  });
+
+  it("fails on ad author does not match user wanting to delete ad", () => {
+    let errorThrown;
+
+    return bcrypt
+      .hash("123123123", 8)
+      .then((hash) => {
+        const user = new User({
+          name: "Li",
+          surname: "Nux",
+          email: "li@nux.com",
+          username: "linux",
+          password: hash,
+        });
+        const ad = new Ad({
+          author: new ObjectId().toString(),
+          title: "Limones",
+          description: "Luneros",
+          price: "10 €/Kg",
+          date: new Date(),
+          adcomments: [],
+        });
+        return Promise.all([user.save(), ad.save()])
+          .then(([savedUser, savedAd]) => {
+            return deleteAd(savedUser.id.toString(), savedAd.id.toString());
+          })
+          .catch((error) => (errorThrown = error));
+      })
+      .finally(() => {
+        expect(errorThrown).to.be.an.instanceOf(MatchError);
+        expect(errorThrown.message).to.equal(
+          "ad author does not match user & cannot be deleted"
+        );
+      });
+  });
+
+  after(() =>
+    Ad.deleteMany()
+      .then(() => User.deleteMany())
+      .then(() => mongoose.disconnect())
+  );
+});
 
 // it('fails on mismatching ad author', () => {
 //     let errorThrown
@@ -176,7 +206,6 @@ describe('deleteAd', () => {
 //             expect(errorThrown.message).to.equal('ad author does not match user & cannot be deleted');
 //         });
 // });
-
 
 /////////////
 
--- staff/eduardo-sanchez/Farm-Hub/Backend/logic/deleteAdComment.js
+++ staff/eduardo-sanchez/Farm-Hub/Backend/logic/deleteAdComment.js
@@ -1,9 +1,9 @@
 import { User, Ad } from '../data/index.js'
 import { SystemError, NotFoundError, MatchError } from 'com/errors.js'
 import validate from 'com/validate.js'
-import { Types } from 'mongoose'
+// import { Types } from 'mongoose'
 
-const { ObjectId } = Types
+// const { ObjectId } = Types
 
 const deleteAdComment = (userId, adId, commentId) => {
     validate.id(userId, 'userId')
@@ -17,39 +17,37 @@ const deleteAdComment = (userId, adId, commentId) => {
                 throw new NotFoundError('ad not found')
             }
 
-            // console.log('Ad found:', ad)
-            // console.log('Looking for comment:', commentId)
-            // console.log('User trying to delete:', userId)
-
+            
             const isCommentOwner = ad.adcomments.some(adcomment => adcomment.author.toString() === userId && adcomment._id.toString() === commentId)
-
+            
             if (!isCommentOwner) {
                 throw new MatchError('comment author does not match user & cannot be deleted')
             }
-
-            // const isCommentOwner = ad.adcomments.some(adcomment => {
-            //     console.log('Comparing:', adcomment.author.toString(), userId, adcomment._id.toString(), commentId)
-            //     return adcomment.author.toString() === userId && adcomment._id.toString() === commentId
-            // })
-
-            // console.log('Is comment owner:', isCommentOwner)
-
-            // if (!isCommentOwner) {
-            //     throw new MatchError('comment author does not match user & cannot be deleted')
-            // }
-
+            
             return Ad.findByIdAndUpdate(adId, { $pull: { adcomments: { _id: commentId } } }).lean()
-                .catch(error => { throw new SystemError(error.message) })
-                .then(ad => {
+            .catch(error => { throw new SystemError(error.message) })
+            .then(ad => {
 
-                    // if (!ad) {
-                    //     throw new NotFoundError('ad not found')
-                    // }
-                    return ad
-
-
+                   return ad
+                        
+                    })
                 })
-        })
-}
-
+            }
+            
 export default deleteAdComment
+            
+
+            // console.log('Ad found:', ad)
+            // console.log('Looking for comment:', commentId)
+// console.log('User trying to delete:', userId)
+            
+  // const isCommentOwner = ad.adcomments.some(adcomment => {
+                //     console.log('Comparing:', adcomment.author.toString(), userId, adcomment._id.toString(), commentId)
+                //     return adcomment.author.toString() === userId && adcomment._id.toString() === commentId
+            // })
+            
+            // console.log('Is comment owner:', isCommentOwner)
+            
+            // if (!isCommentOwner) {
+                //     throw new MatchError('comment author does not match user & cannot be deleted')
+            // }
