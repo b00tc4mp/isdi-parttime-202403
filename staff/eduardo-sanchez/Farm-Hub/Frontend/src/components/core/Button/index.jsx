20264a1a5ca38943cd00d9b075c62dcc79599abd
Eduardolans <eduard.criado@gmail.com>
2024-09-18 19:03:10 +0200

    add default values to the onClick and className props for the custom Button component; #165

--- staff/eduardo-sanchez/Farm-Hub/Frontend/src/components/core/Button/index.jsx
+++ staff/eduardo-sanchez/Farm-Hub/Frontend/src/components/core/Button/index.jsx
@@ -1,12 +1,20 @@
-import "./index.css"
+import './index.css';
 
-export default function Button({ type, children, onClick, className }) {
+export default function Button({
+    type,
+    children,
+    onClick = () => {},
+    className = '',
+}) {
     return (
-
-        <button className={`Button ${className ? className : ''}`} type={type} onClick={onClick}>{children}
+        <button
+            className={`Button ${className ? className : ''}`}
+            type={type}
+            onClick={onClick}
+        >
+            {children}
         </button>
-
-    )
+    );
 }
 
 // export default function Button({ type, children, onClick, className = "" }) {
