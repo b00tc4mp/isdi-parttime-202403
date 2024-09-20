f27cde4ba2e22cc81aad2b2851fd83a0037f071a
Eduardolans <eduard.criado@gmail.com>
2024-09-20 13:23:10 +0200

    add prettier file to the BE project; add conditional in case geolocation is missing in createAd logic; add lat & long object properties in spec tests(createAd & deleteAd; add geolocation in test curl for create-ad; #165

--- staff/eduardo-sanchez/Farm-Hub/Backend/logic/createAd.js
+++ staff/eduardo-sanchez/Farm-Hub/Backend/logic/createAd.js
@@ -1,44 +1,60 @@
-import { User, Ad } from '../data/index.js'
-import { SystemError, NotFoundError } from 'com/errors.js'
-import validate from 'com/validate.js'
-
+import { User, Ad } from '../data/index.js';
+import { SystemError, NotFoundError } from 'com/errors.js';
+import validate from 'com/validate.js';
 
 const createAd = (userId, title, description, price, geoLocation) => {
-    console.log('test', { userId, title, description, price, geoLocation })
-    validate.id(userId, 'userId')
-    validate.text(title, 'title', 50)
-    validate.text(description, 'description', 200)
-    validate.price(price, 'price')
-    validate.geoLocation(geoLocation, 'geoLocation')
+    console.log('test', { userId, title, description, price, geoLocation });
+    validate.id(userId, 'userId');
+    validate.text(title, 'title', 50);
+    validate.text(description, 'description', 200);
+    validate.price(price, 'price');
+    validate.geoLocation(geoLocation, 'geoLocation');
 
-    console.log('geoLocation', geoLocation)
+    console.log('geoLocation', geoLocation);
 
-    return User.findById(userId).lean()
-        .catch(error => { throw new SystemError(error.message) })
-        .then(user => {
-            if (!user) throw new NotFoundError('user not found')
+    if (!geoLocation) {
+        throw new NotFoundError('geoLocation is not valid: missing lat or lng');
+    }
+
+    return User.findById(userId)
+        .lean()
+        .catch((error) => {
+            throw new SystemError(error.message);
+        })
+        .then((user) => {
+            if (!user) throw new NotFoundError('user not found');
+
+            console.log('user: ', user);
 
             const ad = {
                 author: userId,
                 title,
                 description,
                 price,
-                date: new Date,
+                date: new Date(),
                 adcomments: [],
                 geoLocation: {
                     lat: geoLocation.lat,
-                    lng: geoLocation.lng
-                }
+                    lng: geoLocation.lng,
+                },
+            };
 
-            }
-
-            console.log('ad: ', ad)
+            console.log('ad: ', ad);
 
             return Ad.create(ad)
-                .catch(error => { throw new SystemError(error.message) })
-                .then(() => { })
-        })
+                .catch((error) => {
+                    throw new SystemError(error.message);
+                })
+                .then(() => {});
+        });
+};
 
-}
+export default createAd;
 
-export default createAd
+//   if (
+//     typeof geoLocation !== "object" ||
+//     !("lat" in geoLocation) ||
+//     !("lng" in geoLocation)
+//   ) {
+//     throw new NotFoundError(" geolocation is not valid");
+//   }
--- staff/eduardo-sanchez/Farm-Hub/Backend/logic/createAd.spec.js
+++ staff/eduardo-sanchez/Farm-Hub/Backend/logic/createAd.spec.js
@@ -1,131 +1,190 @@
-import 'dotenv/config'
-import mongoose, { Types } from 'mongoose'
-import bcrypt from 'bcryptjs'
-import { expect } from 'chai'
+import 'dotenv/config';
+import mongoose, { Types } from 'mongoose';
+import bcrypt from 'bcryptjs';
+import { expect } from 'chai';
 
-import { User, Ad } from '../data/index.js'
-import createAd from './createAd.js'
-import { NotFoundError, ContentError } from 'com/errors.js'
+import { User, Ad } from '../data/index.js';
+import createAd from './createAd.js';
+import { NotFoundError, ContentError } from 'com/errors.js';
 
-const { MONGODB_URL_TEST } = process.env
+const { MONGODB_URL_TEST } = process.env;
 
-const { ObjectId } = Types
+const { ObjectId } = Types;
 
 describe('createAd', () => {
-    before(() => mongoose.connect(MONGODB_URL_TEST).then(() => Promise.all([User.deleteMany(), Ad.deleteMany()])))
+    before(() =>
+        mongoose
+            .connect(MONGODB_URL_TEST)
+            .then(() => Promise.all([User.deleteMany(), Ad.deleteMany()]))
+    );
 
-    beforeEach(() => Promise.all([User.deleteMany(), Ad.deleteMany()]))
+    beforeEach(() => Promise.all([User.deleteMany(), Ad.deleteMany()]));
 
     it('succeeds on creating an ad from an existing user', () =>
-
-        bcrypt.hash('123123123', 8)
-            .then(hash => User.create({ name: 'Li', surname: 'Nux', email: 'li@nux.com', username: 'linux', password: hash }))
-            .then(user =>
-                createAd(user.id, 'Limones', 'Luneros', '8.5 €/Kg')
-                    .then(() => Ad.findOne())
-                    .then(ad => {
-                        expect(ad.author.toString()).to.equal(user.id.toString())
-                        expect(ad.title).to.equal('Limones')
-                        expect(ad.description).to.equal('Luneros')
-                        expect(ad.price).to.equal('8.5 €/Kg')
-                        expect(ad.date).to.be.a('date')
-                        expect(ad.adcomments).to.be.an('array') //.that.is.empty 
-                    })
+        bcrypt
+            .hash('123123123', 8)
+            .then((hash) =>
+                User.create({
+                    name: 'Li',
+                    surname: 'Nux',
+                    email: 'li@nux.com',
+                    username: 'linux',
+                    password: hash,
+                })
             )
-    )
+            .then((user) =>
+                createAd(user.id, 'Limones', 'Luneros', '8.5 €/Kg', {
+                    lat: 0,
+                    lng: 0,
+                })
+                    .then(() => Ad.findOne())
+                    .then((ad) => {
+                        expect(ad.author.toString()).to.equal(
+                            user.id.toString()
+                        );
+                        expect(ad.title).to.equal('Limones');
+                        expect(ad.description).to.equal('Luneros');
+                        expect(ad.price).to.equal('8.5 €/Kg');
+                        expect(ad.date).to.be.a('date');
+                        expect(ad.adcomments).to.be.an('array'); //.that.is.empty
+                    })
+            ));
 
     it('fails on non-existing user', () => {
-        let errorThrown
+        let errorThrown;
 
-        // return bcrypt.hash("123123123", 8)
-        //     .then(hash => User.create({ name: "Amigo", surname: "Mio", email: "amigo@mio.com", username: "amigomio", password: hash }))
-        //     .then(() => createAd(new ObjectId().toString(), 'Test', 'Funcionando', '8.5 €/Kg', []))
-        //     .catch(error => errorThrown = error)
-        //     .finally(() => {
-        //         expect(errorThrown).to.be.an.instanceOf(NotFoundError)
-        //         expect(errorThrown.message).to.equal("user not found")
-        //     })
-
-        return createAd(new ObjectId().toString(), 'Ad Title', 'Ad Description', '8.5 €/Kg')
-            .catch(error => errorThrown = error)
+        return createAd(
+            new ObjectId().toString(),
+            'Ad Title',
+            'Ad Description',
+            '8.5 €/Kg',
+            { lat: 0, lng: 0 }
+        )
+            .catch((error) => (errorThrown = error))
             .finally(() => {
-                expect(errorThrown).to.be.instanceOf(NotFoundError)
-                expect(errorThrown.message).to.equal('user not found')
-            })
-    })
+                expect(errorThrown).to.be.instanceOf(NotFoundError);
+                expect(errorThrown.message).to.equal('user not found');
+            });
+    });
 
     it('fails on invalid userId', () => {
-        let errorThrown
+        let errorThrown;
 
         try {
-            createAd('invalid-id', 'Ad Title', 'Ad Description', '8.5 €/Kg')
+            createAd('invalid-id', 'Ad Title', 'Ad Description', '8.5 €/Kg', {
+                lat: 0,
+                lng: 0,
+            });
         } catch (error) {
-            errorThrown = error
+            errorThrown = error;
         } finally {
-            expect(errorThrown).to.be.instanceOf(ContentError)
-            expect(errorThrown.message).to.equal('userId is not valid')
+            expect(errorThrown).to.be.instanceOf(ContentError);
+            expect(errorThrown.message).to.equal('userId is not valid');
         }
-    })
-
-    // it('fails on invalid userId', () => {
-    //     expect(() => createAd('invalid-id', 'Ad Title', 'Ad Description', '8.5 €/Kg'))
-    //         .to.throw(ContentError, 'userId is not valid')
-    // })
-
+    });
 
     it('fails on invalid title', () => {
-        let errorThrown
-
+        let errorThrown;
 
         try {
-            createAd(new ObjectId().toString(), '', 'Ad Description', '8.5 €/Kg')
+            createAd(
+                new ObjectId().toString(),
+                '',
+                'Ad Description',
+                '8.5 €/Kg',
+                {
+                    lat: 0,
+                    lng: 0,
+                }
+            );
         } catch (error) {
-            errorThrown = error
+            errorThrown = error;
         } finally {
-            expect(errorThrown).to.be.instanceOf(ContentError)
-            expect(errorThrown.message).to.equal('title is not valid')
+            expect(errorThrown).to.be.instanceOf(ContentError);
+            expect(errorThrown.message).to.equal('title is not valid');
         }
-    })
-
-    // it('fails on invalid title', () => {
-    //     return createAd(new ObjectId().toString(), '', 'Ad Description', '8.5 €/Kg')
-    //         .catch(error => {
-    //             expect(error).to.be.instanceOf(ContentError)
-    //             expect(error.message).to.equal('title is not valid')
-    //         })
-    // })
-
+    });
 
     it('fails on invalid description', () => {
-        let errorThrown
+        let errorThrown;
 
-        // return User.create({ name: 'John', surname: 'Doe', email: 'john@doe.com', username: 'johndoe', password: '12345678' })
-        // .then(user => {
         try {
-            createAd(new ObjectId().toString(), 'Ad Title', '', '8.5 €/Kg')
+            createAd(new ObjectId().toString(), 'Ad Title', '', '8.5 €/Kg', {
+                lat: 0,
+                lng: 0,
+            });
         } catch (error) {
-            errorThrown = error
+            errorThrown = error;
         } finally {
-            expect(errorThrown).to.be.instanceOf(ContentError)
-            expect(errorThrown.message).to.equal('description is not valid')
+            expect(errorThrown).to.be.instanceOf(ContentError);
+            expect(errorThrown.message).to.equal('description is not valid');
         }
-    })
+    });
 
     it('fails on invalid price', () => {
-        let errorThrown
-
-        // return User.create({ name: 'John', surname: 'Doe', email: 'john@doe.com', username: 'johndoe', password: '12345678' })
-        //     .then(user => {
+        let errorThrown;
         try {
-            createAd(new ObjectId().toString(), 'Ad Title', 'Ad Description', '8.5')
+            createAd(
+                new ObjectId().toString(),
+                'Ad Title',
+                'Ad Description',
+                '8.5',
+                {
+                    lat: 0,
+                    lng: 0,
+                }
+            );
         } catch (error) {
-            errorThrown = error
+            errorThrown = error;
         } finally {
-            expect(errorThrown).to.be.instanceOf(ContentError)
-            expect(errorThrown.message).to.equal('price is not valid. It must be in the format "number €/Kg", e.g., "3.20 €/Kg".');
+            expect(errorThrown).to.be.instanceOf(ContentError);
+            expect(errorThrown.message).to.equal(
+                'price is not valid. It must be in the format "number €/Kg", e.g., "3.20 €/Kg".'
+            );
         }
         // });
-    })
+    });
 
-    after(() => Promise.all([User.deleteMany(), Ad.deleteMany()]).then(() => mongoose.disconnect()))
-})
+    it('fails on missing geoLocation', () => {
+        let errorThrown;
+        try {
+            createAd(
+                new ObjectId().toString(),
+                'Ad Title',
+                'Ad Description',
+                '8.5 €/Kg'
+            );
+        } catch (error) {
+            errorThrown = error;
+        } finally {
+            expect(errorThrown).to.be.instanceOf(ContentError);
+            expect(errorThrown.message).to.equal(
+                'geoLocation is not valid: missing lat or lng'
+            );
+        }
+    });
+
+    after(() =>
+        Promise.all([User.deleteMany(), Ad.deleteMany()]).then(() =>
+            mongoose.disconnect()
+        )
+    );
+});
+
+//   it("fails on invalid geoLocation", () => {
+//     let errorThrown;
+//     try {
+//       createAd(
+//         new ObjectId().toString(),
+//         "Ad Title",
+//         "Ad Description",
+//         "8.5 €/Kg",
+//         "invalid"
+//       );
+//     } catch (error) {
+//       errorThrown = error;
+//     } finally {
+//       expect(errorThrown).to.be.instanceOf(ContentError);
+//       expect(errorThrown.message).to.equal("geoLocation is not valid");
+//     }
+//   });
