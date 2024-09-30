832efcca0d6000c10eb536f7aa53076e02af15a4
Eduardolans <eduard.criado@gmail.com>
2024-09-30 16:46:56 +0200

    change dependency to adId in useEffect; add error handlin g to chooseAdtoUpdate function; remove onClick handler from submit button; add contactInfo in request body curl test updateAd; #165

--- staff/eduardo-sanchez/Farm-Hub/Backend/test/update-ad.sh
+++ staff/eduardo-sanchez/Farm-Hub/Backend/test/update-ad.sh
@@ -1,5 +1,2 @@
-curl -X PATCH http://localhost:8080/updatead/66d08d42798fbae9e3ce4205 -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NmIyMjg3NWRmYWVmMWMwYmQ2ZDUzNWUiLCJpYXQiOjE3MjUwMjk2MjEsImV4cCI6MTcyNzYyMTYyMX0.eMeSfUBaLtRJSxny8-3P0_Lcl1BzHknkyUYHzrURdhs" -H "Content-Type: application/json" -d '{"title":"Berengena","description":"MoRADA FiNa", "price":"3.50 €/Kg"}' -v
-
-
-# curl -X PATCH http://localhost:8080/updatead/66d08d42798fbae9e3ce4205 -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NmFlNjM3MGYxMTA4MmVkN2YwNjIzZGQiLCJpYXQiOjE3MjUwMzI2MzIsImV4cCI6MTcyNzYyNDYzMn0.0fybO1hzZVEEubUB-5uVQis3PP4uW-JxioHSjha-UgE" -H "Content-Type: application/json" -d '{"title":"Berengena","description":"MoRADA FiNa", "price":"3.50 €/Kg"}' -v
+curl -X PATCH http://localhost:8080/updatead/66fab7d397be259c43e63c75 -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NmU0ODc5NzMwY2I2M2Y5N2JiYzIzZTUiLCJpYXQiOjE3Mjc3MDcwMTIsImV4cCI6MTczMDI5OTAxMn0.HivghnqiJyTd9H1KSN0NSSLdQ1fdT1KZEP1epvkzGFE" -H "Content-Type: application/json" -d '{"title":"Berengena","description":"MoRADA FiNa", "price":"3.50 €/Kg", "contactInfo":"081-234-5678"}' -v
 
